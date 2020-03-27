39813be5439cf2b2a573ed1c786f3110be0d8ee5
Fnar <fnardottv@gmail.com>
2020-03-26 21:49:06 -0700

    Refactor Creeper Den such that isValidChestSpace can be encapsulated

--- src/main/java/greymerk/roguelike/dungeon/rooms/DungeonsCreeperDen.java
+++ src/main/java/greymerk/roguelike/dungeon/rooms/DungeonsCreeperDen.java
@@ -1,13 +1,11 @@
 package greymerk.roguelike.dungeon.rooms;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 import greymerk.roguelike.dungeon.base.DungeonBase;
 import greymerk.roguelike.dungeon.settings.LevelSettings;
 import greymerk.roguelike.theme.ITheme;
-import greymerk.roguelike.treasure.Treasure;
 import greymerk.roguelike.worldgen.BlockWeightedRandom;
 import greymerk.roguelike.worldgen.Cardinal;
 import greymerk.roguelike.worldgen.Coord;
@@ -18,16 +16,19 @@ import greymerk.roguelike.worldgen.shapes.RectHollow;
 import greymerk.roguelike.worldgen.shapes.RectSolid;
 import greymerk.roguelike.worldgen.spawners.Spawner;
 
+import static greymerk.roguelike.treasure.Treasure.ORE;
+import static greymerk.roguelike.treasure.Treasure.createChest;
+
 public class DungeonsCreeperDen extends DungeonBase {
 
-  public boolean generate(IWorldEditor editor, Random rand, LevelSettings settings, Cardinal[] entrances, Coord origin) {
+  public static final MetaBlock TNT_META_BLOCK = BlockType.get(BlockType.TNT);
+
+  public boolean generate(IWorldEditor editor, Random random, LevelSettings settings, Cardinal[] entrances, Coord origin) {
 
     ITheme theme = settings.getTheme();
 
     Coord start;
     Coord end;
-
-    MetaBlock tnt = BlockType.get(BlockType.TNT);
 
     BlockWeightedRandom mossy = new BlockWeightedRandom();
     mossy.addBlock(theme.getPrimary().getWall(), 3);
@@ -40,25 +41,25 @@ public class DungeonsCreeperDen extends DungeonBase {
 
     BlockWeightedRandom subfloor = new BlockWeightedRandom();
     subfloor.addBlock(floor, 3);
-    subfloor.addBlock(tnt, 1);
+    subfloor.addBlock(TNT_META_BLOCK, 1);
 
     start = new Coord(origin);
     end = new Coord(origin);
     start.add(new Coord(-4, -4, -4));
     end.add(new Coord(4, 5, 4));
-    RectHollow.fill(editor, rand, start, end, mossy, false, true);
+    RectHollow.fill(editor, random, start, end, mossy, false, true);
 
     start = new Coord(origin);
     end = new Coord(origin);
     start.add(new Coord(-3, -1, -3));
     end.add(new Coord(3, -1, 3));
-    RectSolid.fill(editor, rand, start, end, floor, true, true);
+    RectSolid.fill(editor, random, start, end, floor, true, true);
 
     start = new Coord(origin);
     end = new Coord(origin);
     start.add(new Coord(-3, -3, -3));
     end.add(new Coord(3, -2, 3));
-    RectSolid.fill(editor, rand, start, end, subfloor, true, true);
+    RectSolid.fill(editor, random, start, end, subfloor, true, true);
 
     start = new Coord(origin);
     end = new Coord(origin);
@@ -66,26 +67,19 @@ public class DungeonsCreeperDen extends DungeonBase {
     end.add(new Coord(3, 0, 3));
 
     List<Coord> chestSpaces = new RectSolid(start, end).get();
-    Collections.shuffle(chestSpaces, rand);
+    chooseRandomLocations(random, 3, chestSpaces).stream()
+        .peek(chestSpace -> createChest(editor, random, settings.getDifficulty(chestSpace), chestSpace, true, ORE))
+        .forEach(chestSpace -> spawnTntBeneath(editor, chestSpace));
 
-    int counter = 0;
-    for (Coord spot : chestSpaces) {
-      if (Treasure.isValidChestSpace(editor, spot)) {
-        Treasure.createChest(editor, rand, settings.getDifficulty(spot), spot, true, Treasure.ORE);
-        Coord cursor = new Coord(spot);
-        cursor.add(Cardinal.DOWN, 2);
-        tnt.set(editor, cursor);
-        ++counter;
-      }
-
-      if (counter >= 2) {
-        break;
-      }
-    }
-
-    Spawner.generate(editor, rand, settings, new Coord(origin), Spawner.CREEPER);
+    Spawner.generate(editor, random, settings, new Coord(origin), Spawner.CREEPER);
 
     return true;
+  }
+
+  private void spawnTntBeneath(IWorldEditor editor, Coord coord) {
+    Coord cursor = new Coord(coord);
+    cursor.add(Cardinal.DOWN, 2);
+    TNT_META_BLOCK.set(editor, cursor);
   }
 
   public int getSize() {
