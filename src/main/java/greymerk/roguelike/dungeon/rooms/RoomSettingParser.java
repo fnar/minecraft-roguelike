fb3875bbd2d09820dbf22f5f5ef63efbe8693ec8
Fnar <fnardottv@gmail.com>
2020-04-01 00:02:20 -0700

    Improve user experience
    Make supplied values more forgiving if enum casing isn't matched
    Improve logging slightly

--- src/main/java/greymerk/roguelike/dungeon/rooms/RoomSettingParser.java
+++ src/main/java/greymerk/roguelike/dungeon/rooms/RoomSettingParser.java
@@ -7,16 +7,19 @@ import greymerk.roguelike.worldgen.spawners.Spawner;
 
 import static greymerk.roguelike.dungeon.base.DungeonRoom.valueOf;
 import static greymerk.roguelike.dungeon.settings.level.LevelsParser.parseLevelsIfPresent;
+import static java.lang.String.format;
 
 public class RoomSettingParser {
 
-  public static final String SPAWNERS_KEY = "spawners";
+  public static final String NAME_KEY = "name";
+  public static final String ROOM_FREQUENCY = "type";
   public static final String COUNT_KEY = "count";
   public static final String WEIGHT_KEY = "weight";
+  public static final String SPAWNERS_KEY = "spawners";
 
   public static RoomSetting parse(JsonObject roomSettingJson) throws Exception {
     return new RoomSetting(
-        parseDungeonRoom(roomSettingJson),
+        parseName(roomSettingJson),
         parseSpawners(roomSettingJson),
         parseRoomFrequency(roomSettingJson),
         parseWeight(roomSettingJson),
@@ -24,38 +27,42 @@ public class RoomSettingParser {
         parseLevelsIfPresent(roomSettingJson));
   }
 
-  private static DungeonRoom parseDungeonRoom(JsonObject entry) throws Exception {
+  private static DungeonRoom parseName(JsonObject entry) throws Exception {
+    String name = entry.has(NAME_KEY)
+        ? entry.get(NAME_KEY).getAsString().toUpperCase()
+        : "NAME MISSING";
     try {
-      return valueOf(getName(entry));
+      return valueOf(name);
     } catch (IllegalArgumentException e) {
-      throw new Exception("No such dungeon: " + getName(entry));
+      throw new Exception(format("No such room with name %s", name));
     }
+  }
+
+  private static String parseRoomFrequency(JsonObject entry) {
+    return entry.get(ROOM_FREQUENCY).getAsString().toLowerCase();
+  }
+
+  private static int parseCount(JsonObject roomSettingJson) {
+    return roomSettingJson.has(COUNT_KEY)
+        ? roomSettingJson.get(COUNT_KEY).getAsInt()
+        : 1;
+  }
+
+  private static int parseWeight(JsonObject entry) {
+    return entry.has(WEIGHT_KEY)
+        ? entry.get(WEIGHT_KEY).getAsInt()
+        : 1;
   }
 
   private static Spawner parseSpawners(JsonObject entry) throws Exception {
     if (!entry.has(SPAWNERS_KEY)) {
       return null;
     }
+    String spawner = entry.get(SPAWNERS_KEY).getAsString().toUpperCase();
     try {
-      return Spawner.valueOf(entry.get(SPAWNERS_KEY).getAsString().toUpperCase());
+      return Spawner.valueOf(spawner);
     } catch (IllegalArgumentException e) {
-      throw new Exception("No such spawner type: " + getName(entry));
+      throw new Exception(format("No such spawner type %s for room of type %s", spawner, parseName(entry)));
     }
-  }
-
-  private static String getName(JsonObject entry) {
-    return entry.get("name").getAsString().toUpperCase();
-  }
-
-  private static int parseCount(JsonObject roomSettingJson) {
-    return roomSettingJson.has(COUNT_KEY) ? roomSettingJson.get(COUNT_KEY).getAsInt() : 1;
-  }
-
-  private static int parseWeight(JsonObject entry) {
-    return entry.has(WEIGHT_KEY) ? entry.get(WEIGHT_KEY).getAsInt() : 1;
-  }
-
-  private static String parseRoomFrequency(JsonObject entry) {
-    return entry.get("type").getAsString().toLowerCase();
   }
 }
