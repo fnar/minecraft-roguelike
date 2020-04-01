accbfee838669c268ef4479a4928be46a932052f
Samuel Waggoner <samuel.waggoner@gmail.com>
2020-03-31 20:38:39 -0700

    Extract RoomIterator

--- src/main/java/greymerk/roguelike/dungeon/base/DungeonFactory.java
+++ src/main/java/greymerk/roguelike/dungeon/base/DungeonFactory.java
@@ -6,7 +6,6 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 import greymerk.roguelike.dungeon.rooms.RoomSetting;
 import greymerk.roguelike.util.WeightedChoice;
@@ -68,7 +67,7 @@ public class DungeonFactory implements IDungeonFactory {
 
   public IDungeonRoom get(Random rand) {
     if (singleRoomsIterator == null) {
-      singleRoomsIterator = new RoomIterator();
+      singleRoomsIterator = new RoomIterator(singleRoomSettings);
     }
 
     if (singleRoomsIterator.hasNext()) {
@@ -113,26 +112,6 @@ public class DungeonFactory implements IDungeonFactory {
       return false;
     }
     return dungeonRoomWeightedRandomizer.equals(other.dungeonRoomWeightedRandomizer);
-  }
-
-  private class RoomIterator implements Iterator<IDungeonRoom> {
-    private LinkedList<IDungeonRoom> rooms;
-
-    public RoomIterator() {
-      rooms = singleRoomSettings.stream()
-          .map(roomSetting -> getInstance(roomSetting.getDungeonRoom()))
-          .collect(Collectors.toCollection(LinkedList::new));
-    }
-
-    @Override
-    public boolean hasNext() {
-      return !rooms.isEmpty();
-    }
-
-    @Override
-    public IDungeonRoom next() {
-      return rooms.poll();
-    }
   }
 
   @Override
