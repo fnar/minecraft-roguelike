46a4317bad3c652a98f303b7740c57675fb675cb
Fnar <fnardottv@gmail.com>
2020-04-02 04:07:16 +0000

    Exhaustingly refactor SpawnPotential logic
    Probably for detriment

--- src/main/java/greymerk/roguelike/worldgen/spawners/SpawnPotential.java
+++ src/main/java/greymerk/roguelike/worldgen/spawners/SpawnPotential.java
@@ -7,106 +7,118 @@ import java.util.Random;
 
 import greymerk.roguelike.config.RogueConfig;
 import greymerk.roguelike.treasure.loot.Equipment;
-import greymerk.roguelike.treasure.loot.Quality;
 
+import static greymerk.roguelike.treasure.loot.Equipment.AXE;
+import static greymerk.roguelike.treasure.loot.Equipment.CHEST;
+import static greymerk.roguelike.treasure.loot.Equipment.FEET;
+import static greymerk.roguelike.treasure.loot.Equipment.HELMET;
+import static greymerk.roguelike.treasure.loot.Equipment.LEGS;
+import static greymerk.roguelike.treasure.loot.Equipment.PICK;
+import static greymerk.roguelike.treasure.loot.Equipment.SHOVEL;
+import static greymerk.roguelike.treasure.loot.Equipment.getName;
+import static greymerk.roguelike.treasure.loot.Quality.getArmourQuality;
+import static greymerk.roguelike.treasure.loot.Quality.getToolQuality;
 import static greymerk.roguelike.worldgen.spawners.Spawner.SKELETON;
 import static greymerk.roguelike.worldgen.spawners.Spawner.ZOMBIE;
+import static java.util.stream.IntStream.range;
 
 public class SpawnPotential {
 
   private String name;
   private int weight;
   private boolean equip;
-  private NBTTagCompound nbt;
+  private NBTTagCompound entityNbt;
 
-  public SpawnPotential(String name, boolean equip, int weight, NBTTagCompound nbt) {
+  public SpawnPotential(String name, boolean equip, int weight, NBTTagCompound entityNbt) {
     this.name = name;
     this.equip = equip;
     this.weight = weight;
-    this.nbt = nbt;
+    this.entityNbt = entityNbt;
   }
 
-  public NBTTagCompound get(int level) {
-    NBTTagCompound nbt = this.nbt == null ? new NBTTagCompound() : this.nbt.copy();
-    return getPotential(getRoguelike(level, name, nbt));
-  }
-
-  public NBTTagList get(Random rand, int level) {
-
+  public NBTTagList getSpawnPotentials(Random random, int level) {
     NBTTagList potentials = new NBTTagList();
-
-    if (name.equals(ZOMBIE.getName())) {
-      for (int i = 0; i < 24; ++i) {
-        NBTTagCompound mob = new NBTTagCompound();
-        mob = getRoguelike(level, name, mob);
-
-        Equipment tool;
-        switch (rand.nextInt(3)) {
-          case 0:
-            tool = Equipment.SHOVEL;
-            break;
-          case 1:
-            tool = Equipment.AXE;
-            break;
-          case 2:
-            tool = Equipment.PICK;
-            break;
-          default:
-            tool = Equipment.PICK;
-            break;
-        }
-
-        mob = equipHands(mob, Equipment.getName(tool, Quality.getToolQuality(rand, level)), "minecraft:shield");
-        mob = equipArmour(mob, rand, level);
-
-        potentials.appendTag(getPotential(mob));
-      }
-
-      return potentials;
+    if (ZOMBIE.getName().equals(name)) {
+      range(0, 24).forEach(i -> potentials.appendTag(createZombieSpawnPotentialNbt(random, level)));
+    } else if (SKELETON.getName().equals(name)) {
+      range(0, 12).forEach(i -> potentials.appendTag(createSkeletonSpawnPotentialNbt(random, level)));
+    } else {
+      potentials.appendTag(createSpawnPotentialNbt(level));
     }
-
-    if (name.equals(SKELETON.getName())) {
-      for (int i = 0; i < 12; ++i) {
-        NBTTagCompound mob = new NBTTagCompound();
-        mob = getRoguelike(level, name, mob);
-        mob = equipHands(mob, "minecraft:bow", null);
-        mob = equipArmour(mob, rand, level);
-        potentials.appendTag(getPotential(mob));
-      }
-
-      return potentials;
-    }
-
-    potentials.appendTag(getPotential(getRoguelike(level, name, new NBTTagCompound())));
     return potentials;
   }
 
-  private NBTTagCompound getPotential(NBTTagCompound mob) {
-    NBTTagCompound potential = new NBTTagCompound();
-    potential.setTag("Entity", mob);
-    potential.setInteger("Weight", weight);
-    return potential;
+  private NBTTagCompound createZombieSpawnPotentialNbt(Random random, int level) {
+    NBTTagCompound zombieEntityNbt = createZombieEntityNbt(random, level);
+    return buildSpawnPotentialNbt(zombieEntityNbt);
   }
 
-  private NBTTagCompound equipHands(NBTTagCompound mob, String weapon, String offhand) {
+  private NBTTagCompound createSkeletonSpawnPotentialNbt(Random random, int level) {
+    NBTTagCompound skeletonEntityNbt = createSkeletonEntityNbt(random, level);
+    return buildSpawnPotentialNbt(skeletonEntityNbt);
+  }
+
+  private NBTTagCompound createSpawnPotentialNbt(int level) {
+    NBTTagCompound entityNbt = createEntityNbt(level);
+    return buildSpawnPotentialNbt(entityNbt);
+  }
+
+  private NBTTagCompound createZombieEntityNbt(Random random, int level) {
+    NBTTagCompound entityNbt = createEntityNbt(level);
+    equipHands(entityNbt, random, level);
+    equipArmour(entityNbt, random, level);
+    return entityNbt;
+  }
+
+  private NBTTagCompound createSkeletonEntityNbt(Random random, int level) {
+    NBTTagCompound entityNbt = createEntityNbt(level);
+    equipHands(entityNbt, "minecraft:bow", null);
+    equipArmour(entityNbt, random, level);
+    return entityNbt;
+  }
+
+  private NBTTagCompound createEntityNbt(int level) {
+    // This copy of entityNbt is non-obvious but important. It's so that there can be a variety of spawn potentials for Zombies/skeletons/whatever
+    NBTTagCompound entityNbt = this.entityNbt.copy();
+    setRoguelikeNbtData(level, name, entityNbt);
+    return entityNbt;
+  }
+
+  private void equipHands(NBTTagCompound entityNbt, Random random, int level) {
+    String mainHand = getName(chooseRandomTool(random), getToolQuality(random, level));
+    String offHand = "minecraft:shield";
+    equipHands(entityNbt, mainHand, offHand);
+  }
+
+  private Equipment chooseRandomTool(Random random) {
+    return random.nextBoolean()
+        ? PICK
+        : random.nextBoolean()
+            ? AXE
+            : SHOVEL;
+  }
+
+  private NBTTagCompound buildSpawnPotentialNbt(NBTTagCompound entityNbt) {
+    NBTTagCompound spawnPotentialNbt = new NBTTagCompound();
+    spawnPotentialNbt.setTag("Entity", entityNbt);
+    spawnPotentialNbt.setInteger("Weight", weight);
+    return spawnPotentialNbt;
+  }
+
+  private void equipHands(NBTTagCompound entityNbt, String mainHand, String offhand) {
     NBTTagList hands = new NBTTagList();
-    hands.appendTag(getItem(weapon));
+    hands.appendTag(getItem(mainHand));
     hands.appendTag(getItem(offhand));
-    mob.setTag("HandItems", hands);
-
-    return mob;
+    entityNbt.setTag("HandItems", hands);
   }
 
-  private NBTTagCompound equipArmour(NBTTagCompound mob, Random rand, int level) {
-
+  private void equipArmour(NBTTagCompound entityNbt, Random rand, int level) {
     NBTTagList armour = new NBTTagList();
-    armour.appendTag(getItem(Equipment.getName(Equipment.FEET, Quality.getArmourQuality(rand, level))));
-    armour.appendTag(getItem(Equipment.getName(Equipment.LEGS, Quality.getArmourQuality(rand, level))));
-    armour.appendTag(getItem(Equipment.getName(Equipment.CHEST, Quality.getArmourQuality(rand, level))));
-    armour.appendTag(getItem(Equipment.getName(Equipment.HELMET, Quality.getArmourQuality(rand, level))));
-    mob.setTag("ArmorItems", armour);
-
-    return mob;
+    armour.appendTag(getItem(getName(FEET, getArmourQuality(rand, level))));
+    armour.appendTag(getItem(getName(LEGS, getArmourQuality(rand, level))));
+    armour.appendTag(getItem(getName(CHEST, getArmourQuality(rand, level))));
+    armour.appendTag(getItem(getName(HELMET, getArmourQuality(rand, level))));
+    entityNbt.setTag("ArmorItems", armour);
   }
 
   private NBTTagCompound getItem(String itemName) {
@@ -119,27 +131,24 @@ public class SpawnPotential {
     return item;
   }
 
-  private NBTTagCompound getRoguelike(int level, String type, NBTTagCompound tag) {
-
-    tag.setString("id", type);
+  private void setRoguelikeNbtData(int level, String type, NBTTagCompound entityNbt) {
+    entityNbt.setString("id", type);
 
     if (!(RogueConfig.getBoolean(RogueConfig.ROGUESPAWNERS)
         && equip)) {
-      return tag;
+      return;
     }
 
-    NBTTagList activeEffects = new NBTTagList();
-    tag.setTag("ActiveEffects", activeEffects);
-
     NBTTagCompound buff = new NBTTagCompound();
-    activeEffects.appendTag(buff);
-
     buff.setByte("Id", (byte) 4);
     buff.setByte("Amplifier", (byte) level);
     buff.setInteger("Duration", 10);
     buff.setByte("Ambient", (byte) 0);
 
-    return tag;
+    NBTTagList activeEffects = new NBTTagList();
+    activeEffects.appendTag(buff);
+
+    entityNbt.setTag("ActiveEffects", activeEffects);
   }
 
 }
