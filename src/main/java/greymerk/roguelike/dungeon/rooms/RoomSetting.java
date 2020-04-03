6ae20ff65f22c4e3672be04fd4c3a3fef9eaa635
Fnar <fnardottv@gmail.com>
2020-04-03 03:19:29 +0000

    Employ Lombok to simplify RoomSetting

--- src/main/java/greymerk/roguelike/dungeon/rooms/RoomSetting.java
+++ src/main/java/greymerk/roguelike/dungeon/rooms/RoomSetting.java
@@ -1,12 +1,19 @@
 package greymerk.roguelike.dungeon.rooms;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 import greymerk.roguelike.dungeon.base.DungeonRoom;
 import greymerk.roguelike.worldgen.spawners.Spawner;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@Getter
 public class RoomSetting {
 
   private DungeonRoom dungeonRoom;
@@ -17,78 +24,15 @@ public class RoomSetting {
   private int count;
   private List<Integer> levels;
 
-  public RoomSetting(
-      DungeonRoom dungeonRoom,
-      Spawner spawner,
-      String spawnerId,
-      String frequency,
-      int weight,
-      int count,
-      List<Integer> levels
-  ) {
-    this.dungeonRoom = dungeonRoom;
-    this.spawner = spawner;
-    this.spawnerId = spawnerId;
-    this.frequency = frequency;
-    this.weight = weight;
-    this.count = count;
-    this.levels = levels;
-  }
-
-  public DungeonRoom getDungeonRoom() {
-    return dungeonRoom;
-  }
-
   public Optional<Spawner> getSpawner() {
     return Optional.ofNullable(spawner);
-  }
-
-  public String getFrequency() {
-    return frequency;
   }
 
   public boolean isSecret() {
     return "secret".equals(getFrequency());
   }
 
-  public int getWeight() {
-    return weight;
-  }
-
-  public int getCount() {
-    return count;
-  }
-
-  public List<Integer> getLevels() {
-    return levels;
-  }
-
   public boolean isOnFloorLevel(int floorLevel) {
     return getLevels() != null && getLevels().contains(floorLevel);
-  }
-
-  public String getSpawnerId() {
-    return spawnerId;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    // I think this is only for tests, which means the behaviour isn't being tested, just the state
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    RoomSetting that = (RoomSetting) o;
-    return weight == that.weight &&
-        dungeonRoom == that.dungeonRoom &&
-        spawner == that.spawner &&
-        Objects.equals(frequency, that.frequency);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(dungeonRoom, spawner, frequency, weight);
   }
 }
