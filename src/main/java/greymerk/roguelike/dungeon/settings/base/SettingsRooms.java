c377a8b98b6d74bc3a9c793816ac21eae5eca67b
Samuel Waggoner <samuel.waggoner@gmail.com>
2020-05-13 18:25:49 -0700

    Refactor Base Room Settings, adding Rewards rooms, specifically, such that modded loot appears more frequently

--- src/main/java/greymerk/roguelike/dungeon/settings/base/SettingsRooms.java
+++ src/main/java/greymerk/roguelike/dungeon/settings/base/SettingsRooms.java
@@ -1,6 +1,11 @@
 package greymerk.roguelike.dungeon.settings.base;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 import greymerk.roguelike.dungeon.base.RoomsSetting;
+import greymerk.roguelike.dungeon.rooms.RoomSetting;
 import greymerk.roguelike.dungeon.settings.DungeonSettings;
 import greymerk.roguelike.dungeon.settings.LevelSettings;
 import greymerk.roguelike.dungeon.settings.SettingIdentifier;
@@ -21,6 +26,7 @@ import static greymerk.roguelike.dungeon.base.RoomType.OBSIDIAN;
 import static greymerk.roguelike.dungeon.base.RoomType.OSSUARY;
 import static greymerk.roguelike.dungeon.base.RoomType.PIT;
 import static greymerk.roguelike.dungeon.base.RoomType.PRISON;
+import static greymerk.roguelike.dungeon.base.RoomType.REWARD;
 import static greymerk.roguelike.dungeon.base.RoomType.SLIME;
 import static greymerk.roguelike.dungeon.base.RoomType.SPIDER;
 
@@ -31,67 +37,83 @@ public class SettingsRooms extends DungeonSettings {
   public SettingsRooms() {
     super(ID);
     setExclusive(false);
-    for (int i = 0; i < 5; ++i) {
+    addRoomsToLevel(0, getLevel0Rooms());
+    addRoomsToLevel(1, getLevel1Rooms());
+    addRoomsToLevel(2, getLevel2Rooms());
+    addRoomsToLevel(3, getLevel3Rooms());
+    addRoomsToLevel(4, getLevel4Rooms());
+  }
 
-      RoomsSetting factory;
+  private void addRoomsToLevel(int i, List<RoomSetting> rooms) {
+    RoomsSetting roomSetting = new RoomsSetting();
+    rooms.forEach(roomSetting::add);
 
-      switch (i) {
-        case 0:
-          factory = new RoomsSetting();
-          factory.add(CAKE.newSingleRoomSetting());
-          factory.add(FIRE.newSingleRoomSetting());
-          factory.add(BRICK.newRandomRoomSetting(4));
-          factory.add(CORNER.newRandomRoomSetting(2));
-          break;
-        case 1:
-          factory = new RoomsSetting();
-          factory.add(PIT.newSingleRoomSetting());
-          factory.add(CORNER.newRandomRoomSetting(10));
-          factory.add(BRICK.newRandomRoomSetting(3));
-          break;
-        case 2:
-          factory = new RoomsSetting();
-          factory.add(OSSUARY.newSingleRoomSetting());
-          factory.add(CRYPT.newSingleRoomSetting());
-          factory.add(CREEPER.newSingleRoomSetting());
-          factory.add(FIRE.newSingleRoomSetting());
-          factory.add(SPIDER.newSingleRoomSetting());
-          factory.add(PRISON.newSingleRoomSetting());
-          factory.add(CRYPT.newRandomRoomSetting(5));
-          factory.add(CORNER.newRandomRoomSetting(5));
-          factory.add(BRICK.newRandomRoomSetting(3));
-          break;
-        case 3:
-          factory = new RoomsSetting();
-          factory.add(OSSUARY.newSingleRoomSetting());
-          factory.add(ENDER.newSingleRoomSetting());
-          factory.add(CRYPT.newSingleRoomSetting());
-          factory.add(PRISON.newRandomRoomSetting(3));
-          factory.add(SLIME.newRandomRoomSetting(5));
-          factory.add(CREEPER.newRandomRoomSetting(1));
-          factory.add(SPIDER.newRandomRoomSetting(1));
-          factory.add(PIT.newRandomRoomSetting(1));
-          break;
-        case 4:
-          factory = new RoomsSetting();
-          factory.add(OBSIDIAN.newSingleRoomSetting());
-          factory.add(BLAZE.newSingleRoomSetting());
-          factory.add(PRISON.newSingleRoomSetting());
-          factory.add(DARKHALL.newSingleRoomSetting());
-          factory.add(FORTRESS.newSingleRoomSetting());
-          factory.add(SLIME.newRandomRoomSetting(10));
-          factory.add(BLAZE.newRandomRoomSetting(3));
-          factory.add(NETHER.newRandomRoomSetting(3));
-          factory.add(SPIDER.newRandomRoomSetting(2));
-          break;
-        default:
-          factory = new RoomsSetting();
-          break;
-      }
+    LevelSettings level = new LevelSettings();
+    level.setRooms(roomSetting);
 
-      LevelSettings level = new LevelSettings();
-      level.setRooms(factory);
-      getLevels().put(i, level);
-    }
+    getLevels().put(i, level);
+  }
+
+  private List<RoomSetting> getLevel0Rooms() {
+    return Lists.newArrayList(
+        CAKE.newSingleRoomSetting(),
+        FIRE.newSingleRoomSetting(),
+        BRICK.newRandomRoomSetting(4),
+        CORNER.newRandomRoomSetting(2),
+        REWARD.newSingleRoomSetting()
+    );
+  }
+
+  private List<RoomSetting> getLevel1Rooms() {
+    return Lists.newArrayList(
+        PIT.newSingleRoomSetting(),
+        CORNER.newRandomRoomSetting(10),
+        BRICK.newRandomRoomSetting(3),
+        REWARD.newSingleRoomSetting()
+    );
+  }
+
+  private List<RoomSetting> getLevel2Rooms() {
+    return Lists.newArrayList(
+        OSSUARY.newSingleRoomSetting(),
+        CRYPT.newSingleRoomSetting(),
+        CREEPER.newSingleRoomSetting(),
+        FIRE.newSingleRoomSetting(),
+        SPIDER.newSingleRoomSetting(),
+        PRISON.newSingleRoomSetting(),
+        CRYPT.newRandomRoomSetting(5),
+        CORNER.newRandomRoomSetting(5),
+        BRICK.newRandomRoomSetting(3),
+        REWARD.newSingleRoomSetting()
+    );
+  }
+
+  private List<RoomSetting> getLevel3Rooms() {
+    return Lists.newArrayList(
+        OSSUARY.newSingleRoomSetting(),
+        ENDER.newSingleRoomSetting(),
+        CRYPT.newSingleRoomSetting(),
+        PRISON.newRandomRoomSetting(3),
+        SLIME.newRandomRoomSetting(5),
+        CREEPER.newRandomRoomSetting(1),
+        SPIDER.newRandomRoomSetting(1),
+        PIT.newRandomRoomSetting(1),
+        REWARD.newSingleRoomSetting()
+    );
+  }
+
+  private List<RoomSetting> getLevel4Rooms() {
+    return Lists.newArrayList(
+        OBSIDIAN.newSingleRoomSetting(),
+        BLAZE.newSingleRoomSetting(),
+        PRISON.newSingleRoomSetting(),
+        DARKHALL.newSingleRoomSetting(),
+        FORTRESS.newSingleRoomSetting(),
+        SLIME.newRandomRoomSetting(10),
+        BLAZE.newRandomRoomSetting(3),
+        NETHER.newRandomRoomSetting(3),
+        SPIDER.newRandomRoomSetting(2),
+        REWARD.newSingleRoomSetting()
+    );
   }
 }
