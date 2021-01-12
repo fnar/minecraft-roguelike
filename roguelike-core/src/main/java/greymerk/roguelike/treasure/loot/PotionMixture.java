a28c530865838ef2a8c87dee0bbff4382c4af078
Samuel Waggoner <samuel.waggoner@gmail.com>
2021-01-11 23:23:34 -0800

    Slowly push Forge's ItemStack further away

--- roguelike-core/src/main/java/greymerk/roguelike/treasure/loot/PotionMixture.java
+++ roguelike-core/src/main/java/greymerk/roguelike/treasure/loot/PotionMixture.java
@@ -1,12 +1,12 @@
 package greymerk.roguelike.treasure.loot;
 
+import com.github.srwaggon.minecraft.item.RldItemStack;
 import com.github.srwaggon.minecraft.item.potion.Amplification;
 import com.github.srwaggon.minecraft.item.potion.Effect;
 import com.github.srwaggon.minecraft.item.potion.EffectType;
 import com.github.srwaggon.minecraft.item.potion.Potion;
 import com.github.srwaggon.minecraft.item.potion.PotionItemStackMapper;
 
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 import java.util.Random;
@@ -30,6 +30,14 @@ public enum PotionMixture {
   public static final PotionMixture[] POTIONS = {LAUDANUM, RAGE, STAMINA, NECTAR, COFFEE, AURA};
   public static final PotionMixture[] BOOZE = {TEQUILA, LAUDANUM, MOONSHINE, ABSINTHE, STOUT};
 
+  public static ItemStack chooseRandomBooze(Random rand) {
+    return getPotion(rand, chooseRandomAmong(rand, BOOZE));
+  }
+
+  public static ItemStack chooseRandomPotion(Random rand) {
+    return getPotion(rand, chooseRandomAmong(rand, POTIONS));
+  }
+
   public static PotionMixture chooseRandom(Random random) {
     return chooseRandomAmong(random, values());
   }
@@ -39,6 +47,11 @@ public enum PotionMixture {
   }
 
   public static ItemStack getPotion(Random random, PotionMixture type) {
+    RldItemStack rldItemStack = getPotionAsRldItemStack(random, type);
+    return PotionItemStackMapper.map(rldItemStack);
+  }
+
+  public static RldItemStack getPotionAsRldItemStack(Random random, PotionMixture type) {
     switch (type) {
       case TEQUILA:
         return getTequila(random);
@@ -48,6 +61,7 @@ public enum PotionMixture {
         return getMoonshine(random);
       case ABSINTHE:
         return getAbsinthe();
+      default:
       case VILE:
         return getVile(random);
       case RAGE:
@@ -62,23 +76,21 @@ public enum PotionMixture {
         return getCoffee();
       case AURA:
         return getAura();
-      default:
     }
-    return new ItemStack(Items.GLASS_BOTTLE);
   }
 
-  public static ItemStack getTequila(Random random) {
-    return PotionItemStackMapper.map(Potion.newPotion()
+   public static RldItemStack getTequila(Random random) {
+    return Potion.newPotion()
         .withEffect(Effect.newEffect(EffectType.STRENGTH, 2, 30 + random.nextInt(60)))
         .withEffect(Effect.newEffect(EffectType.FATIGUE, 0, 30 + random.nextInt(60)))
         .asItemStack()
         .withDisplayName("Tequila")
         .withHideFlag(ItemHideFlags.EFFECTS)
-        .withTag("CustomPotionColor", DyeColor.RGBToColor(255, 232, 196)));
+        .withTag("CustomPotionColor", DyeColor.RGBToColor(255, 232, 196));
   }
 
-  public static ItemStack getLaudanum() {
-    return PotionItemStackMapper.map(Potion.newPotion()
+  public static RldItemStack getLaudanum() {
+    return Potion.newPotion()
         .withEffect(Effect.newEffect(EffectType.REGEN, 2, 8))
         .withEffect(Effect.newEffect(EffectType.WEAKNESS, 1, 5))
         .withEffect(Effect.newEffect(EffectType.SLOWNESS, 1, 5))
@@ -88,11 +100,11 @@ public enum PotionMixture {
         .withDisplayName("Laudanum")
         .withDisplayLore("A medicinal tincture.")
         .withHideFlag(ItemHideFlags.EFFECTS)
-        .withTag("CustomPotionColor", DyeColor.RGBToColor(150, 50, 0)));
+        .withTag("CustomPotionColor", DyeColor.RGBToColor(150, 50, 0));
   }
 
-  public static ItemStack getMoonshine(Random random) {
-    return PotionItemStackMapper.map(
+  public static RldItemStack getMoonshine(Random random) {
+    return
         Potion.newPotion()
             .withEffect(Effect.newEffect(EffectType.DAMAGE, 0, 1))
             .withEffect(Effect.newEffect(EffectType.BLINDNESS, 0, 30 + random.nextInt(60)))
@@ -100,11 +112,11 @@ public enum PotionMixture {
             .asItemStack()
             .withDisplayName("Moonshine")
             .withHideFlag(ItemHideFlags.EFFECTS)
-            .withTag("CustomPotionColor", DyeColor.RGBToColor(250, 240, 230)));
+            .withTag("CustomPotionColor", DyeColor.RGBToColor(250, 240, 230));
   }
 
-  public static ItemStack getAbsinthe() {
-    return PotionItemStackMapper.map(
+  public static RldItemStack getAbsinthe() {
+    return
         Potion.newPotion()
             .withEffect(Effect.newEffect(EffectType.POISON, 0, 3))
             .withEffect(Effect.newEffect(EffectType.NIGHT_VISION, 0, 120))
@@ -112,13 +124,13 @@ public enum PotionMixture {
             .asItemStack()
             .withDisplayName("Absinthe")
             .withHideFlag(ItemHideFlags.EFFECTS)
-            .withTag("CustomPotionColor", DyeColor.RGBToColor(200, 250, 150)));
+            .withTag("CustomPotionColor", DyeColor.RGBToColor(200, 250, 150));
   }
 
-  public static ItemStack getVile(Random random) {
+  public static RldItemStack getVile(Random random) {
     EffectType randomEffect0 = EffectType.chooseRandom(random);
     EffectType randomEffect1 = EffectType.chooseRandom(random);
-    return PotionItemStackMapper.map(
+    return
         Potion.newPotion()
             .withForm(PotionForm.chooseRandom(random))
             .withType(PotionType.chooseRandom(random))
@@ -134,12 +146,11 @@ public enum PotionMixture {
                 .withDuration(getSuggestedDuration(random, randomEffect1)))
             .asItemStack()
             .withDisplayName("Vile Mixture")
-            .withHideFlag(ItemHideFlags.EFFECTS)
-    );
+            .withHideFlag(ItemHideFlags.EFFECTS);
   }
 
-  public static ItemStack getRage() {
-    return PotionItemStackMapper.map(
+  public static RldItemStack getRage() {
+    return
         Potion.newPotion()
             .withEffect(Effect.newEffect(EffectType.STRENGTH, 2, 20))
             .withEffect(Effect.newEffect(EffectType.BLINDNESS, 0, 10))
@@ -148,11 +159,11 @@ public enum PotionMixture {
             .withDisplayName("Animus")
             .withDisplayLore("An unstable mixture.")
             .withHideFlag(ItemHideFlags.EFFECTS)
-            .withTag("CustomPotionColor", DyeColor.RGBToColor(255, 0, 0)));
+            .withTag("CustomPotionColor", DyeColor.RGBToColor(255, 0, 0));
   }
 
-  public static ItemStack getStamina() {
-    return PotionItemStackMapper.map(
+  public static RldItemStack getStamina() {
+    return
         Potion.newPotion()
             .withEffect(Effect.newEffect(EffectType.SATURATION, 9, 1))
             .withEffect(Effect.newEffect(EffectType.SPEED, 1, 120))
@@ -162,11 +173,11 @@ public enum PotionMixture {
             .withDisplayName("Vitae")
             .withDisplayLore("Essence of life.")
             .withHideFlag(ItemHideFlags.EFFECTS)
-            .withTag("CustomPotionColor", DyeColor.RGBToColor(230, 50, 20)));
+            .withTag("CustomPotionColor", DyeColor.RGBToColor(230, 50, 20));
   }
 
-  public static ItemStack getStout() {
-    return PotionItemStackMapper.map(
+  public static RldItemStack getStout() {
+    return
         Potion.newPotion()
             .withEffect(Effect.newEffect(EffectType.REGEN, 0, 5))
             .withEffect(Effect.newEffect(EffectType.SATURATION, 1, 1))
@@ -176,11 +187,11 @@ public enum PotionMixture {
             .withDisplayName("Stout")
             .withDisplayLore("\"It's Good for You\"")
             .withHideFlag(ItemHideFlags.EFFECTS)
-            .withTag("CustomPotionColor", DyeColor.RGBToColor(50, 40, 20)));
+            .withTag("CustomPotionColor", DyeColor.RGBToColor(50, 40, 20));
   }
 
-  public static ItemStack getNectar() {
-    return PotionItemStackMapper.map(
+  public static RldItemStack getNectar() {
+    return
         Potion.newPotion()
             .withEffect(Effect.newEffect(EffectType.ABSORPTION, 9, 20))
             .withEffect(Effect.newEffect(EffectType.RESISTANCE, 2, 20))
@@ -189,11 +200,11 @@ public enum PotionMixture {
             .withDisplayName("Nectar")
             .withDisplayLore("A Floral extract.")
             .withHideFlag(ItemHideFlags.EFFECTS)
-            .withTag("CustomPotionColor", DyeColor.RGBToColor(250, 150, 250)));
+            .withTag("CustomPotionColor", DyeColor.RGBToColor(250, 150, 250));
   }
 
-  public static ItemStack getCoffee() {
-    return PotionItemStackMapper.map(
+  public static RldItemStack getCoffee() {
+    return
         Potion.newPotion()
             .withEffect(Effect.newEffect(EffectType.HASTE, 1, 600))
             .withEffect(Effect.newEffect(EffectType.SPEED, 0, 600))
@@ -201,26 +212,18 @@ public enum PotionMixture {
             .withDisplayName("Coffee")
             .withDisplayLore("A darkroast bean brew.")
             .withHideFlag(ItemHideFlags.EFFECTS)
-            .withTag("CustomPotionColor", DyeColor.RGBToColor(20, 20, 10)));
+            .withTag("CustomPotionColor", DyeColor.RGBToColor(20, 20, 10));
   }
 
-  public static ItemStack getAura() {
-    return PotionItemStackMapper.map(
+  public static RldItemStack getAura() {
+    return
         Potion.newPotion()
             .withEffect(Effect.newEffect(EffectType.GLOWING, 0, 600))
             .asItemStack()
             .withDisplayName("Luma")
             .withDisplayLore("A glowstone extract.")
             .withHideFlag(ItemHideFlags.EFFECTS)
-            .withTag("CustomPotionColor", DyeColor.RGBToColor(250, 250, 0)));
-  }
-
-  public static ItemStack getBooze(Random rand) {
-    return getPotion(rand, chooseRandomAmong(rand, BOOZE));
-  }
-
-  public static ItemStack getRandom(Random rand) {
-    return getPotion(rand, chooseRandomAmong(rand, POTIONS));
+            .withTag("CustomPotionColor", DyeColor.RGBToColor(250, 250, 0));
   }
 
   public static int getSuggestedDuration(Random random, EffectType type) {
