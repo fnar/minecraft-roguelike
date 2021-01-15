a411c7e764414c6de752cda834ee919917ccac1d
Fnar <fnardottv@gmail.com>
2021-01-15 12:56:03 -0800

    Minor tidying

--- roguelike-core/src/main/java/com/github/srwaggon/minecraft/item/BlockItemMapper1_12.java
+++ roguelike-core/src/main/java/com/github/srwaggon/minecraft/item/BlockItemMapper1_12.java
@@ -3,12 +3,17 @@ package com.github.srwaggon.minecraft.item;
 import com.github.srwaggon.minecraft.block.BlockMapper1_12;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 import greymerk.roguelike.worldgen.MetaBlock1_2;
 
 public class BlockItemMapper1_12 {
 
-  public static Item mapBlock(BlockItem item) {
+  public static ItemStack map(RldItemStack rldItemStack) {
+    return new ItemStack(map((BlockItem)rldItemStack.getItem()));
+  }
+
+  private static Item map(BlockItem item) {
     MetaBlock1_2 metaBlock = BlockMapper1_12.map(item.getBlockType());
     return Item.getItemFromBlock(metaBlock.getBlock());
   }
--- roguelike-core/src/main/java/greymerk/roguelike/treasure/loot/RecordSong.java
+++ roguelike-core/src/main/java/greymerk/roguelike/treasure/loot/RecordSong.java
@@ -1,10 +1,5 @@
 package greymerk.roguelike.treasure.loot;
 
-import com.github.srwaggon.minecraft.item.RecordItem;
-import com.github.srwaggon.minecraft.item.RecordMapper1_12;
-
-import net.minecraft.item.ItemStack;
-
 import java.util.Random;
 
 public enum RecordSong {
@@ -22,9 +17,8 @@ public enum RecordSong {
   ELEVEN,
   WAIT;
 
-  public static ItemStack getRandomRecord(Random rand) {
-    RecordSong song = RecordSong.values()[rand.nextInt(RecordSong.values().length)];
-    return RecordMapper1_12.map(RecordItem.newRecord().withSong(song));
+  public static RecordSong chooseRandom(Random random) {
+    return RecordSong.values()[random.nextInt(RecordSong.values().length)];
   }
 
 }
