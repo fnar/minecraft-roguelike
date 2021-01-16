eba418b5fcf622ca99ffed2b82d58b64cf4a2836
Samuel Waggoner <samuel.waggoner@gmail.com>
2021-01-15 17:15:28 -0800

    Encapsulate, Rename

--- roguelike-core/src/main/java/greymerk/roguelike/treasure/loot/provider/ItemRecord.java
+++ roguelike-core/src/main/java/greymerk/roguelike/treasure/loot/provider/ItemRecord.java
@@ -1,13 +1,11 @@
 package greymerk.roguelike.treasure.loot.provider;
 
 import com.github.srwaggon.minecraft.item.ItemMapper1_12;
-import com.github.srwaggon.minecraft.item.RecordItem;
+import com.github.srwaggon.minecraft.item.Record;
 
 import net.minecraft.item.ItemStack;
 
 import java.util.Random;
-
-import greymerk.roguelike.treasure.loot.RecordSong;
 
 public class ItemRecord extends ItemBase {
 
@@ -17,9 +15,7 @@ public class ItemRecord extends ItemBase {
 
   @Override
   public ItemStack getLootItem(Random rand, int level) {
-    RecordSong song = RecordSong.chooseRandom(rand);
-    return ItemMapper1_12.map(RecordItem.newRecord().withSong(song).asItemStack());
+    Record.Song song = Record.Song.chooseRandom(rand);
+    return ItemMapper1_12.map(Record.newRecord().withSong(song).asItemStack());
   }
-
-
 }
