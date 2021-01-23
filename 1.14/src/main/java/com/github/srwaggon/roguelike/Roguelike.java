289b652fbcc302f2b2159cbaeaad49266c41140a
Samuel Waggoner <samuel.waggoner@gmail.com>
2021-01-23 15:41:56 -0800

    🎉 Bump version to 2.3.0

--- 1.14/src/main/java/com/github/srwaggon/roguelike/Roguelike.java
+++ 1.14/src/main/java/com/github/srwaggon/roguelike/Roguelike.java
@@ -26,8 +26,8 @@ import java.util.stream.Collectors;
 @Mod("roguelike")
 public class Roguelike {
 
-  public static final String version = "2.2.1";
-  public static final String date = "2020/11/20";
+  public static final String version = "2.3.0";
+  public static final String date = "2021/1/23";
   // The instance of your mod that Forge uses.
 //  @Instance("roguelike")
 //  public static Roguelike instance;
