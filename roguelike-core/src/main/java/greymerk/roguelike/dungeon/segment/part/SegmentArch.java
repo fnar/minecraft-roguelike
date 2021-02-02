746a3a8fab3bbf96715711c8d29e7cbf1ea5d859
Fnar <fnardottv@gmail.com>
2021-02-02 01:34:18 -0800

    🧹 Code cleaning, but changes segment tops to use pillars
    Fixes #48

--- roguelike-core/src/main/java/greymerk/roguelike/dungeon/segment/part/SegmentArch.java
+++ roguelike-core/src/main/java/greymerk/roguelike/dungeon/segment/part/SegmentArch.java
@@ -10,6 +10,7 @@ import greymerk.roguelike.theme.ThemeBase;
 import greymerk.roguelike.worldgen.Coord;
 import greymerk.roguelike.worldgen.Direction;
 import greymerk.roguelike.worldgen.WorldEditor;
+import greymerk.roguelike.worldgen.shapes.RectSolid;
 
 public class SegmentArch extends SegmentBase {
 
@@ -19,8 +20,8 @@ public class SegmentArch extends SegmentBase {
     StairsBlock stair = theme.getSecondary().getStair();
     stair.setUpsideDown(true).setFacing(dir.reverse());
 
-    Coord cursor = origin.copy();
-    cursor.translate(dir, 2);
+    Coord cursor = origin.copy()
+        .translate(dir, 2);
     SingleBlockBrush.AIR.stroke(editor, cursor);
     cursor.up(1);
     SingleBlockBrush.AIR.stroke(editor, cursor);
@@ -28,16 +29,11 @@ public class SegmentArch extends SegmentBase {
     stair.stroke(editor, cursor);
 
     for (Direction orthogonals : dir.orthogonals()) {
-      cursor = origin.copy();
-      cursor.translate(orthogonals, 1);
-      cursor.translate(dir, 2);
-      theme.getSecondary().getPillar().stroke(editor, cursor);
-      cursor.up(1);
-      theme.getSecondary().getPillar().stroke(editor, cursor);
-      cursor.up(1);
-      theme.getPrimary().getWall().stroke(editor, cursor);
-      cursor.translate(dir.reverse(), 1);
-      stair.stroke(editor, cursor);
+      Coord pillarBase = origin.copy().translate(dir, 2).translate(orthogonals, 1);
+      Coord pillarTop = pillarBase.copy().up(2);
+      RectSolid.newRect(pillarBase, pillarTop).fill(editor, theme.getSecondary().getPillar());
+      pillarTop.translate(dir.reverse(), 1);
+      stair.stroke(editor, pillarTop);
     }
   }
 }
