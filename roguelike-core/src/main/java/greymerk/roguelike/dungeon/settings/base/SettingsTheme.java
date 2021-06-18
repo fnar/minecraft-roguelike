9f612462aeb303085d251d56fdb0c5406614429a
Fnar <fnardottv@gmail.com>
2021-06-18 00:37:10 -0700

    Diversify dungeons' fourth-level themes

--- roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/base/SettingsTheme.java
+++ roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/base/SettingsTheme.java
@@ -7,22 +7,29 @@ import greymerk.roguelike.dungeon.settings.SettingsContainer;
 import greymerk.roguelike.dungeon.settings.TowerSettings;
 import greymerk.roguelike.dungeon.towers.Tower;
 import greymerk.roguelike.theme.Theme;
+import greymerk.roguelike.theme.ThemeBase;
 
 public class SettingsTheme extends DungeonSettings {
 
   public static final SettingIdentifier ID = new SettingIdentifier(SettingsContainer.BUILTIN_NAMESPACE, "theme");
 
+  private final static Theme[] themes = {Theme.OAK, Theme.SPRUCE, Theme.CRYPT, Theme.MOSSY};
+  private final static Theme[] level4Themes = {Theme.CAVE, Theme.HELL, Theme.ICE, Theme.NETHER, Theme.NETHER_FORTRESS};
+
   public SettingsTheme() {
     super(ID);
 
     setTowerSettings(new TowerSettings(Tower.ROGUE, Theme.TOWER));
-
-    Theme[] themes = {Theme.OAK, Theme.SPRUCE, Theme.CRYPT, Theme.MOSSY, Theme.HELL};
-
     for (int i = 0; i < 5; ++i) {
       LevelSettings level = new LevelSettings();
-      level.setTheme(themes[i].getThemeBase());
+      ThemeBase theme = i == 4 ? randomTheme().getThemeBase() : themes[i].getThemeBase();
+      level.setTheme(theme);
       getLevelSettings().put(i, level);
     }
+  }
+
+  private Theme randomTheme() {
+    int choice = (int) (Math.random() * SettingsTheme.level4Themes.length);
+    return SettingsTheme.level4Themes[choice];
   }
 }
