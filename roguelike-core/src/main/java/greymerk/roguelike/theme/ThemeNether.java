247c7adb89caa33da7f522aa852087e0de6c6f30
Fnar <fnardottv@gmail.com>
2021-06-18 00:36:41 -0700

    Rework ice theme and nether theme

--- roguelike-core/src/main/java/greymerk/roguelike/theme/ThemeNether.java
+++ roguelike-core/src/main/java/greymerk/roguelike/theme/ThemeNether.java
@@ -5,7 +5,6 @@ import com.github.srwaggon.minecraft.block.normal.StairsBlock;
 import com.github.srwaggon.minecraft.block.redstone.DoorBlock;
 
 import greymerk.roguelike.config.RogueConfig;
-import greymerk.roguelike.worldgen.BlockBrush;
 import greymerk.roguelike.worldgen.BlockWeightedRandom;
 
 public class ThemeNether extends ThemeBase {
@@ -13,10 +12,9 @@ public class ThemeNether extends ThemeBase {
   public ThemeNether() {
 
     BlockWeightedRandom walls = new BlockWeightedRandom();
-    walls.addBlock(BlockType.NETHERBRICK.getBrush(), 200);
-    walls.addBlock(BlockType.NETHERRACK.getBrush(), 20);
-    walls.addBlock(BlockType.ORE_QUARTZ.getBrush(), 20);
-    walls.addBlock(BlockType.SOUL_SAND.getBrush(), 15);
+    walls.addBlock(BlockType.NETHERRACK.getBrush(), 200);
+    walls.addBlock(BlockType.ORE_QUARTZ.getBrush(), 30);
+    walls.addBlock(BlockType.SOUL_SAND.getBrush(), 30);
     walls.addBlock(BlockType.COAL_BLOCK.getBrush(), 5);
 
     BlockWeightedRandom floor = new BlockWeightedRandom();
@@ -29,14 +27,22 @@ public class ThemeNether extends ThemeBase {
       floor.addBlock(BlockType.DIAMOND_BLOCK.getBrush(), 1);
     }
 
-    StairsBlock stair = StairsBlock.netherBrick();
-    BlockBrush pillar = BlockType.OBSIDIAN.getBrush();
-    DoorBlock door = DoorBlock.iron();
-    BlockBrush lightstone = BlockType.GLOWSTONE.getBrush();
-    BlockBrush liquid = BlockType.LAVA_FLOWING.getBrush();
+    this.primary = new BlockSet(
+        floor,
+        walls,
+        StairsBlock.netherBrick(),
+        BlockType.OBSIDIAN.getBrush(),
+        DoorBlock.iron(),
+        BlockType.GLOWSTONE.getBrush(),
+        BlockType.LAVA_FLOWING.getBrush());
 
-    this.primary = new BlockSet(floor, walls, stair, pillar, door, lightstone, liquid);
-    this.secondary = this.primary;
-
+  this.secondary = new BlockSet(
+        floor,
+        walls,
+        StairsBlock.netherBrick(),
+        BlockType.NETHERBRICK.getBrush(),
+        DoorBlock.iron(),
+        BlockType.GLOWSTONE.getBrush(),
+        BlockType.LAVA_FLOWING.getBrush());
   }
 }
