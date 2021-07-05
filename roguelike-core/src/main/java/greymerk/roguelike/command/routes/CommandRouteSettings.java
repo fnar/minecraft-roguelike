dba8ca313148836448579281faa5bce83e84b39a
Fnar <fnardottv@gmail.com>
2021-07-05 16:38:54 -0700

    ♻ Begin refactor around abstracting commands

--- roguelike-core/src/main/java/greymerk/roguelike/command/routes/CommandRouteSettings.java
+++ roguelike-core/src/main/java/greymerk/roguelike/command/routes/CommandRouteSettings.java
@@ -1,10 +1,13 @@
 package greymerk.roguelike.command.routes;
 
+import com.github.fnar.roguelike.command.ReloadSettingsCommand;
+import com.github.fnar.roguelike.command.SimpleRoguelikeCommand;
+
 import java.util.List;
+import java.util.function.Consumer;
 
 import greymerk.roguelike.command.CommandContext;
 import greymerk.roguelike.command.CommandRouteBase;
-import greymerk.roguelike.dungeon.Dungeon;
 import greymerk.roguelike.dungeon.settings.SettingsResolver;
 import greymerk.roguelike.util.ArgumentParser;
 
@@ -12,34 +15,39 @@ public class CommandRouteSettings extends CommandRouteBase {
 
   @Override
   public void execute(CommandContext context, List<String> args) {
-    ArgumentParser ap = new ArgumentParser(args);
+    ArgumentParser argumentParser = new ArgumentParser(args);
 
-    if (!ap.hasEntry(0)) {
+    if (!argumentParser.hasEntry(0)) {
       context.sendInfo("Usage: roguelike settings [reload | list]");
       return;
     }
-    if (ap.match(0, "reload")) {
-      try {
-        Dungeon.initResolver();
-        context.sendSuccess("Settings Reloaded");
-      } catch (Exception exception) {
-        context.sendFailure(exception);
-      }
-      return;
+
+    if (argumentParser.match(0, "reload")) {
+      new ReloadSettingsCommand(
+          () -> context.sendSuccess("Settings reloaded successfully."),
+          context::sendFailure
+      ).run();
     }
 
-    if (ap.match(0, "list")) {
-      try {
-        SettingsResolver settingsResolver = SettingsResolver.initSettingsResolver();
-        if (ap.hasEntry(1)) {
-          String namespace = ap.get(1);
-          context.sendSuccess(settingsResolver.toString(namespace));
-          return;
+    if (argumentParser.match(0, "list")) {
+      String namespace = argumentParser.hasEntry(1) ? argumentParser.get(1) : "";
+
+      Consumer<Exception> onException = context::sendFailure;
+      Runnable onSuccess = () -> context.sendSuccess("Settings listed successfully.");
+      Runnable onRun = () -> {
+        try {
+          SettingsResolver settingsResolver = SettingsResolver.initSettingsResolver();
+          if (namespace.isEmpty()) {
+            context.sendInfo(settingsResolver.toString());
+          } else {
+            context.sendInfo(settingsResolver.toString(namespace));
+          }
+        } catch (Exception exception) {
+          onException.accept(exception);
         }
-        context.sendSuccess(settingsResolver.toString());
-      } catch (Exception exception) {
-        context.sendFailure(exception);
-      }
+      };
+      new SimpleRoguelikeCommand(onRun, onSuccess, onException).run();
     }
   }
+
 }
