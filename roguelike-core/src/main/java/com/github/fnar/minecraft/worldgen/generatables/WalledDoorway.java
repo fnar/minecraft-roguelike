e9c6e8194e6045c95de6d085faf701de0fe8ef30
Fnar <fnardottv@gmail.com>
2021-07-05 17:17:01 -0700

    Widen walled doorways

--- roguelike-core/src/main/java/com/github/fnar/minecraft/worldgen/generatables/WalledDoorway.java
+++ roguelike-core/src/main/java/com/github/fnar/minecraft/worldgen/generatables/WalledDoorway.java
@@ -11,8 +11,8 @@ public class WalledDoorway implements Generatable {
 
   public void generate(WorldEditor worldEditor, LevelSettings levelSettings, Coord origin, Direction facing) {
     RectSolid rect = RectSolid.newRect(
-        origin.copy().translate(facing.left()),
-        origin.copy().translate(facing.right()).up(2)
+        origin.copy().translate(facing.left(), 2),
+        origin.copy().translate(facing.right(), 2).up(2)
     );
 
     BlockSet blockSet = worldEditor.getRandom().nextBoolean()
