fnar/minecraft-roguelike

commit 56d7607e693e8eb276791cafde8a33efb2e8a002

🧹 Decouple Direction from net.minecraft (Forge)

1.12/src/main/java/com/github/fnar/minecraft/block/BlockMapper1_12.java
@@ -83,7 +83,7 @@ public class BlockMapper1_12 {
   public static MetaBlock1_12 map(SingleBlockBrush block) {
     Direction facing = block.getFacing();
 
-    EnumFacing enumFacing = facing.getFacing();
+    EnumFacing enumFacing = getFacing(facing);
 
     switch (block.getBlockType()) {
       case ANVIL:
@@ -246,13 +246,13 @@ public class BlockMapper1_12 {
         return getInfestedBlock((InfestedBlock) block);
       case CHEST:
         return new MetaBlock1_12(Blocks.CHEST)
-            .withProperty(BlockChest.FACING, facing.getFacing());
+            .withProperty(BlockChest.FACING, enumFacing);
       case TRAPPED_CHEST:
         return new MetaBlock1_12(Blocks.TRAPPED_CHEST)
-            .withProperty(BlockChest.FACING, facing.getFacing());
+            .withProperty(BlockChest.FACING, enumFacing);
       case NETHER_PORTAL:
         return new MetaBlock1_12(Blocks.PORTAL)
-            .withProperty(BlockPortal.AXIS, facing.getFacing().getAxis());
+            .withProperty(BlockPortal.AXIS, enumFacing.getAxis());
       default:
         return map(block.getBlockType());
     }
@@ -283,12 +283,12 @@ public class BlockMapper1_12 {
   private static MetaBlock1_12 getRepeater(RepeaterBlock block) {
     return new MetaBlock1_12(block.isPowered() ? Blocks.POWERED_REPEATER : Blocks.UNPOWERED_REPEATER)
         .withProperty(BlockRedstoneRepeater.DELAY, block.getDelay().asInt())
-        .withProperty(BlockRedstoneRepeater.FACING, block.getFacing().getFacing());
+        .withProperty(BlockRedstoneRepeater.FACING, getFacing(block.getFacing()));
   }
 
   private static MetaBlock1_12 getComparator(ComparatorBlock block) {
     return new MetaBlock1_12(block.isPowered() ? Blocks.POWERED_COMPARATOR : Blocks.UNPOWERED_COMPARATOR)
-        .withProperty(BlockRedstoneComparator.FACING, block.getFacing().getFacing())
+        .withProperty(BlockRedstoneComparator.FACING, getFacing(block.getFacing()))
         .withProperty(BlockRedstoneComparator.MODE, block.getMode() == ComparatorBlock.Mode.SUBTRACTION
             ? BlockRedstoneComparator.Mode.SUBTRACT
             : BlockRedstoneComparator.Mode.COMPARE);
@@ -296,7 +296,7 @@ public class BlockMapper1_12 {
 
   private static MetaBlock1_12 getCocoaBlock(Direction facing) {
     return new MetaBlock1_12(Blocks.COCOA)
-        .withProperty(BlockCocoa.FACING, facing.reverse().getFacing())
+        .withProperty(BlockCocoa.FACING, getFacing(facing.reverse()))
         .withProperty(BlockCocoa.AGE, 2);
   }
 
@@ -308,7 +308,25 @@ public class BlockMapper1_12 {
             ? BlockLever.EnumOrientation.UP_X
             : dir == Direction.DOWN
                 ? BlockLever.EnumOrientation.DOWN_X
-                : dir.reverse().getOrientation());
+                : getLeverOrientation(dir.reverse()));
+  }
+
+  public static BlockLever.EnumOrientation getLeverOrientation(Direction direction) {
+    switch(direction) {
+      default:
+      case NORTH:
+        return BlockLever.EnumOrientation.NORTH;
+      case EAST:
+        return BlockLever.EnumOrientation.EAST;
+      case SOUTH:
+        return BlockLever.EnumOrientation.SOUTH;
+      case WEST:
+        return BlockLever.EnumOrientation.WEST;
+      case UP:
+        return BlockLever.EnumOrientation.UP_X;
+      case DOWN:
+        return BlockLever.EnumOrientation.DOWN_X;
+    }
   }
 
   // TODO: can all public calls to this be replaced with calls to map(SingleBlockBrush)?
@@ -605,13 +623,13 @@ public class BlockMapper1_12 {
 
   private static MetaBlock1_12 getBed(BedBlock bedBlock) {
     return new MetaBlock1_12(Blocks.BED)
-        .withProperty(BlockBed.FACING, bedBlock.getFacing().getFacing())
+        .withProperty(BlockBed.FACING, getFacing(bedBlock.getFacing()))
         .withProperty(BlockBed.PART, bedBlock.isHead() ? BlockBed.EnumPartType.HEAD : BlockBed.EnumPartType.FOOT);
   }
 
   private static MetaBlock1_12 getPumpkin(PumpkinBlock block) {
     return new MetaBlock1_12(block.isLit() ? Blocks.LIT_PUMPKIN : Blocks.PUMPKIN)
-        .withProperty(BlockPumpkin.FACING, block.getFacing().getFacing());
+        .withProperty(BlockPumpkin.FACING, getFacing(block.getFacing()));
   }
 
   private static MetaBlock1_12 getAnvil(AnvilBlock block) {
@@ -620,7 +638,7 @@ public class BlockMapper1_12 {
     }
     return new MetaBlock1_12(Blocks.ANVIL)
         .withProperty(BlockAnvil.DAMAGE, block.getDamage().ordinal())
-        .withProperty(BlockAnvil.FACING, block.getFacing().getFacing());
+        .withProperty(BlockAnvil.FACING, getFacing(block.getFacing()));
   }
 
   public static MetaBlock1_12 mapStairs(StairsBlock block) {
@@ -629,7 +647,7 @@ public class BlockMapper1_12 {
         ? new MetaBlock1_12(json)
         : new MetaBlock1_12(getBlockForStairs(block.getBlockType()));
     return metaBlock1_12
-        .withProperty(BlockStairs.FACING, block.getFacing().getFacing())
+        .withProperty(BlockStairs.FACING, getFacing(block.getFacing()))
         .withProperty(BlockStairs.HALF, block.isUpsideDown() ? BlockStairs.EnumHalf.TOP : BlockStairs.EnumHalf.BOTTOM);
   }
 
@@ -671,7 +689,7 @@ public class BlockMapper1_12 {
     return new MetaBlock1_12(block.getMaterial() == Material.METAL ? Blocks.IRON_DOOR : Blocks.TRAPDOOR)
         .withProperty(BlockTrapDoor.HALF, block.isFlushWithTop() ? BlockTrapDoor.DoorHalf.TOP : BlockTrapDoor.DoorHalf.BOTTOM)
         .withProperty(BlockTrapDoor.OPEN, block.isOpen())
-        .withProperty(BlockTrapDoor.FACING, block.getFacing().getFacing());
+        .withProperty(BlockTrapDoor.FACING, getFacing(block.getFacing()));
   }
 
   public static MetaBlock1_12 mapDoor(DoorBlock block) {
@@ -681,7 +699,7 @@ public class BlockMapper1_12 {
         : getDoorMetaBlock(block.getBlockType());
     return doorMetaBlock
         .withProperty(BlockDoor.HALF, block.isTop() ? BlockDoor.EnumDoorHalf.UPPER : BlockDoor.EnumDoorHalf.LOWER)
-        .withProperty(BlockDoor.FACING, block.getFacing().getFacing())
+        .withProperty(BlockDoor.FACING, getFacing(block.getFacing()))
         .withProperty(BlockDoor.OPEN, block.isOpen())
         .withProperty(BlockDoor.HINGE, block.isHingeLeft() ? BlockDoor.EnumHingePosition.LEFT : BlockDoor.EnumHingePosition.RIGHT);
   }
@@ -809,7 +827,7 @@ public class BlockMapper1_12 {
         ? EnumFacing.UP
         : dir == Direction.DOWN
             ? EnumFacing.DOWN
-            : dir.reverse().getFacing();
+            : getFacing(dir.reverse());
 
     return new MetaBlock1_12(minecraftTorchBlock)
         .withProperty(BlockTorch.FACING, facing);
@@ -1127,7 +1145,7 @@ public class BlockMapper1_12 {
   private static MetaBlock1_12 getTallPlant(TallPlantBlock tallPlantBlock) {
     return new MetaBlock1_12(Blocks.DOUBLE_PLANT)
         .withProperty(BlockDoublePlant.VARIANT, getTallPlantMinecraftBlock(tallPlantBlock.getTallPlant()))
-        .withProperty(BlockDoublePlant.FACING, tallPlantBlock.getFacing().getFacing())
+        .withProperty(BlockDoublePlant.FACING, getFacing(tallPlantBlock.getFacing()))
         .withProperty(BlockDoublePlant.HALF, tallPlantBlock.isTop()
             ? BlockDoublePlant.EnumBlockHalf.UPPER
             : BlockDoublePlant.EnumBlockHalf.LOWER);
@@ -1148,6 +1166,24 @@ public class BlockMapper1_12 {
         return BlockDoublePlant.EnumPlantType.ROSE;
       case PEONY:
         return BlockDoublePlant.EnumPlantType.PAEONIA;
+    }
+  }
+
+  public static EnumFacing getFacing(Direction direction) {
+    switch (direction) {
+      default:
+      case NORTH:
+        return EnumFacing.NORTH;
+      case EAST:
+        return EnumFacing.EAST;
+      case SOUTH:
+        return EnumFacing.SOUTH;
+      case WEST:
+        return EnumFacing.WEST;
+      case UP:
+        return EnumFacing.UP;
+      case DOWN:
+        return EnumFacing.DOWN;
     }
   }
 }

1.12/src/main/java/com/github/fnar/minecraft/PositionInfo1_12.java
@@ -9,8 +9,8 @@ import greymerk.roguelike.worldgen.PositionInfo;
 
 public class PositionInfo1_12 implements PositionInfo {
 
-  private World world;
-  private Coord pos;
+  private final World world;
+  private final Coord pos;
 
   public PositionInfo1_12(World world, Coord pos) {
     this.world = world;

1.12/src/main/java/com/github/fnar/minecraft/WorldEditor1_12.java
@@ -293,9 +293,9 @@ public class WorldEditor1_12 implements WorldEditor {
 
   @Override
   public boolean canPlace(SingleBlockBrush block, Coord pos, Direction dir) {
-    // todo: can we map the `block` herea instead of block.getType()?
+    // todo: can we map the `block` here instead of block.getType()?
     return isAirBlock(pos)
-        && BlockMapper1_12.map(block.getBlockType()).getBlock().canPlaceBlockOnSide(world, getBlockPos(pos), dir.getFacing());
+        && BlockMapper1_12.map(block.getBlockType()).getBlock().canPlaceBlockOnSide(world, getBlockPos(pos), BlockMapper1_12.getFacing(dir));
   }
 
   @Override

roguelike-core/src/main/java/greymerk/roguelike/worldgen/Direction.java
@@ -3,9 +3,6 @@ package greymerk.roguelike.worldgen;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
-import net.minecraft.block.BlockLever.EnumOrientation;
-import net.minecraft.util.EnumFacing;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
@@ -14,26 +11,19 @@ import java.util.Set;
 
 public enum Direction {
 
-  NORTH(EnumFacing.SOUTH, EnumOrientation.SOUTH, 0, 0, -1),
-  EAST(EnumFacing.WEST, EnumOrientation.WEST, 1, 0, 0),
-  SOUTH(EnumFacing.NORTH, EnumOrientation.NORTH, 0, 0, 1),
-  WEST(EnumFacing.EAST, EnumOrientation.EAST, -1, 0, 0),
-  UP(EnumFacing.UP, EnumOrientation.UP_X, 0, 1, 0),
-  DOWN(EnumFacing.DOWN, EnumOrientation.DOWN_X, 0, -1, 0),
+  NORTH(0, 0, -1),
+  EAST(1, 0, 0),
+  SOUTH(0, 0, 1),
+  WEST(-1, 0, 0),
+  UP(0, 1, 0),
+  DOWN(0, -1, 0),
   ;
 
-  private final EnumFacing facing;
-  private final EnumOrientation orientation;
   private final int xDelta;
   private final int yDelta;
   private final int zDelta;
 
-  Direction(
-      EnumFacing facing,
-      EnumOrientation orientation,
-      int xDelta, int yDelta, int zDelta) {
-    this.facing = facing;
-    this.orientation = orientation;
+  Direction(int xDelta, int yDelta, int zDelta) {
     this.xDelta = xDelta;
     this.yDelta = yDelta;
     this.zDelta = zDelta;
@@ -41,14 +31,6 @@ public enum Direction {
 
   public static Direction randomCardinal(Random random) {
     return CARDINAL.get(random.nextInt(CARDINAL.size()));
-  }
-
-  public EnumFacing getFacing() {
-    return facing;
-  }
-
-  public EnumOrientation getOrientation() {
-    return orientation;
   }
 
   public static List<Direction> CARDINAL = Collections.unmodifiableList(Lists.newArrayList(NORTH, EAST, SOUTH, WEST));

commit a189f5f290ba0554bec2f9cc88a40cc856bd45a5

🐛 Fix issue where blocks are facing the wrong way
Bug was introduced in 56d7607e693e8eb276791cafde8a33efb2e8a002.

1.12/src/main/java/com/github/fnar/minecraft/block/BlockMapper1_12.java
@@ -1171,16 +1171,18 @@ public class BlockMapper1_12 {
   }
 
   public static EnumFacing getFacing(Direction direction) {
+    // NOTE: I notice these are flipped but I'm not entirely sure why.
+    // *** If you "fix" them, then all of the blocks in the dungeon are reversed.
     switch (direction) {
       default:
       case NORTH:
-        return EnumFacing.NORTH;
-      case EAST:
-        return EnumFacing.EAST;
-      case SOUTH:
         return EnumFacing.SOUTH;
-      case WEST:
+      case EAST:
         return EnumFacing.WEST;
+      case SOUTH:
+        return EnumFacing.NORTH;
+      case WEST:
+        return EnumFacing.EAST;
       case UP:
         return EnumFacing.UP;
       case DOWN: