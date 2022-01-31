0167101cb7338c8e34b423cac958a618fe7782ca
Fnar <fnardottv@gmail.com>
2022-01-31 13:12:38 -0800

    🎀 Sort the contents of Statistics book by descending block count

--- roguelike-core/src/main/java/greymerk/roguelike/treasure/loot/books/BookStatistics.java
+++ roguelike-core/src/main/java/greymerk/roguelike/treasure/loot/books/BookStatistics.java
@@ -3,8 +3,11 @@ package greymerk.roguelike.treasure.loot.books;
 import com.github.fnar.minecraft.block.BlockType;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.ToIntFunction;
 
 import greymerk.roguelike.treasure.loot.Book;
 import greymerk.roguelike.worldgen.WorldEditor;
@@ -25,22 +28,27 @@ public class BookStatistics extends Book {
     List<String> pages = new ArrayList<>();
     Map<BlockType, Integer> stats = editor.getStats();
 
-    int counter = 0;
-    StringBuilder page = new StringBuilder();
-    for (BlockType type : stats.keySet()) {
-      int count = stats.get(type);
-//      String name = abbreviate(type.getLocalizedName(), 16);
-      String name = abbreviate(type.toString(), 16);
-      String line = name + " : " + count + "\n";
-      page.append(line);
-      ++counter;
-      if (counter == 12) {
-        counter = 0;
-        pages.add(page.toString());
-        page = new StringBuilder();
-      }
-    }
-    pages.add(page.toString());
+    final int[] counter = {0};
+    final StringBuilder[] page = {new StringBuilder()};
+    Set<Map.Entry<BlockType, Integer>> entries = stats.entrySet();
+    entries.stream()
+        .sorted(Comparator.comparingInt((ToIntFunction<Map.Entry<BlockType, Integer>>) Map.Entry::getValue).reversed())
+        .forEach((entry) -> {
+          BlockType blockType = entry.getKey();
+          Integer count = entry.getValue();
+
+          String name = abbreviate(blockType.toString(), 16);
+          String line = name + ": " + count + "\n";
+          page[0].append(line);
+          ++counter[0];
+          if (counter[0] == 12) {
+            counter[0] = 0;
+            pages.add(page[0].toString());
+            page[0] = new StringBuilder();
+          }
+        });
+
+    pages.add(page[0].toString());
     return pages;
   }
 }
