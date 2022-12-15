f7cf8d61a6f2421e83aa1d096469018a1bb61924
Fnar <fnardottv@gmail.com>
2022-12-14 19:20:14 -0800

    Modify Netherfort loot (to garden, supplies, and tools)

--- roguelike-core/src/main/java/greymerk/roguelike/treasure/loot/provider/GardenLootItem.java
+++ roguelike-core/src/main/java/greymerk/roguelike/treasure/loot/provider/GardenLootItem.java
@@ -3,6 +3,8 @@ package greymerk.roguelike.treasure.loot.provider;
 import com.google.common.collect.Lists;
 
 import com.github.fnar.minecraft.block.BlockType;
+import com.github.fnar.minecraft.item.Dye;
+import com.github.fnar.minecraft.item.Material;
 import com.github.fnar.minecraft.item.RldItem;
 import com.github.fnar.minecraft.item.RldItemStack;
 import com.github.fnar.minecraft.material.Crop;
@@ -10,6 +12,8 @@ import com.github.fnar.minecraft.material.Crop;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+
+import greymerk.roguelike.util.DyeColor;
 
 public class GardenLootItem extends LootItem {
 
@@ -24,6 +28,8 @@ public class GardenLootItem extends LootItem {
     Arrays.stream(Crop.values()).map(Crop::asSeed).map(RldItem::asStack).forEach(items::add);
     items.add(BlockType.PUMPKIN.asItem().asStack());
     items.add(BlockType.WHEAT.asItem().asStack());
+    items.add(Material.clayBall().asStack());
+    items.add(new Dye(DyeColor.WHITE).asStack());
 
     return items.get(random.nextInt(items.size()));
   }
