e6041f25d843bfa21b211fa374590f612d932465
Fnar <fnardottv@gmail.com>
2022-12-17 12:28:45 -0800

    ♻ Refactor DungeonsSlime.java into component functions

--- roguelike-core/src/main/java/greymerk/roguelike/dungeon/rooms/prototype/DungeonsSlime.java
+++ roguelike-core/src/main/java/greymerk/roguelike/dungeon/rooms/prototype/DungeonsSlime.java
@@ -8,7 +8,6 @@ import java.util.List;
 import greymerk.roguelike.dungeon.base.BaseRoom;
 import greymerk.roguelike.dungeon.rooms.RoomSetting;
 import greymerk.roguelike.dungeon.settings.LevelSettings;
-import greymerk.roguelike.worldgen.BlockBrush;
 import greymerk.roguelike.worldgen.Coord;
 import greymerk.roguelike.worldgen.Direction;
 import greymerk.roguelike.worldgen.WorldEditor;
@@ -22,121 +21,152 @@ public class DungeonsSlime extends BaseRoom {
   }
 
   public BaseRoom generate(Coord origin, List<Direction> entrances) {
-    BlockBrush bars = BlockType.IRON_BAR.getBrush();
+    Coord start = origin.copy().translate(new Coord(-8, -1, -8));
+    Coord end = origin.copy().translate(new Coord(8, 5, 8));
+    walls().fill(worldEditor, RectHollow.newRect(start, end), false, true);
 
-    Coord start = origin.copy();
-    Coord end = origin.copy();
-    start.translate(new Coord(-8, -1, -8));
-    end.translate(new Coord(8, 5, 8));
-    RectHollow.newRect(start, end).fill(worldEditor, walls(), false, true);
-
-    Coord cursor;
-    for (Direction dir : Direction.CARDINAL) {
-      cursor = origin.copy();
-      cursor.translate(dir, 5);
-      cursor.translate(dir.antiClockwise(), 5);
-      corner(cursor);
-
-      start = origin.copy();
-      start.up(4);
-      start.translate(dir, 3);
-      end = start.copy();
-      start.translate(dir.antiClockwise(), 8);
-      end.translate(dir.clockwise(), 8);
-      RectSolid.newRect(start, end).fill(worldEditor, walls());
-      start.translate(dir, 4);
-      end.translate(dir, 4);
-      RectSolid.newRect(start, end).fill(worldEditor, walls());
-
-    }
-
-
-    for (Direction dir : Direction.CARDINAL) {
-      if (!entrances.contains(dir)) {
-        start = origin.copy();
-        start.translate(dir, 4);
-        end = start.copy();
-        end.translate(dir, 2);
-        start.translate(dir.antiClockwise(), 3);
-        end.translate(dir.clockwise(), 3);
-        RectSolid.newRect(start, end).fill(worldEditor, SingleBlockBrush.AIR);
-        start.down();
-        end.down();
-        RectSolid.newRect(start, end).fill(worldEditor, liquid());
-        start.down();
-        end.down();
-        RectSolid.newRect(start, end).fill(worldEditor, walls());
-
-        start = origin.copy();
-        start.translate(dir, 3);
-        end = start.copy();
-        start.translate(dir.antiClockwise(), 2);
-        end.translate(dir.clockwise(), 2);
-        RectSolid.newRect(start, end).fill(worldEditor, bars);
-
-        cursor = origin.copy();
-        cursor.translate(dir, 7);
-        walls().stroke(worldEditor, cursor);
-        cursor.up(2);
-        walls().stroke(worldEditor, cursor);
-        cursor.down();
-        cursor.translate(dir);
-        liquid().stroke(worldEditor, cursor);
-        for (Direction o : dir.orthogonals()) {
-          cursor = origin.copy();
-          cursor.translate(dir, 7);
-          cursor.translate(o);
-          stairs().setUpsideDown(true).setFacing(o).stroke(worldEditor, cursor);
-          cursor.up();
-          walls().stroke(worldEditor, cursor);
-          cursor.up();
-          stairs().setUpsideDown(false).setFacing(o).stroke(worldEditor, cursor);
-
-        }
-      }
-    }
-
+    generateCorners(origin);
+    generateWalls(origin);
+    generateWaterways(origin, entrances);
+    generateRailings(origin, entrances);
+    generatePipes(origin, entrances);
 
     return this;
   }
 
-  private void corner(Coord origin) {
-    BlockBrush bars = BlockType.IRON_BAR.getBrush();
-
-    Coord start = origin.copy();
-    Coord end = origin.copy();
-    start.translate(new Coord(-1, -1, -1));
-    end.translate(new Coord(1, -1, 1));
-    RectSolid.newRect(start, end).fill(worldEditor, liquid());
-
-    start = origin.copy();
-    end = origin.copy();
-    start.translate(new Coord(-1, -2, -1));
-    end.translate(new Coord(1, -2, 1));
-    RectSolid.newRect(start, end).fill(worldEditor, walls());
-
+  private void generateCorners(Coord origin) {
     for (Direction dir : Direction.CARDINAL) {
-      start = origin.copy();
-      start.translate(dir, 2);
-      start.translate(dir.antiClockwise(), 2);
-      end = start.copy();
-      end.up(3);
-      RectSolid.newRect(start, end).fill(worldEditor, pillars());
+      Coord cornerCoord = origin.copy();
+      cornerCoord.translate(dir, 5);
+      cornerCoord.translate(dir.antiClockwise(), 5);
+      generateCorner(cornerCoord);
+    }
+  }
+
+  private void generateCorner(Coord origin) {
+    generateCornerPool(origin);
+    generateCornerPillars(origin);
+    generateCornerExternalRailings(origin);
+  }
+
+  private void generateCornerPool(Coord origin) {
+    Coord start = origin.copy().translate(-1, -1, -1);
+    Coord end = origin.copy().translate(1, -1, 1);
+    RectSolid poolCoords = RectSolid.newRect(start, end);
+    liquid().fill(worldEditor, poolCoords);
+    poolCoords.translate(Direction.DOWN, 1);
+    walls().fill(worldEditor, poolCoords);
+  }
+
+  private void generateCornerPillars(Coord origin) {
+    for (Direction dir : Direction.CARDINAL) {
+      Coord start = origin.copy()
+          .translate(dir, 2)
+          .translate(dir.antiClockwise(), 2);
+      Coord end = start.copy().up(3);
+      pillars().fill(worldEditor, RectSolid.newRect(start, end));
 
       for (Direction d : Direction.CARDINAL) {
         Coord cursor = end.copy();
         cursor.translate(d);
         stairs().setUpsideDown(true).setFacing(d).stroke(worldEditor, cursor, true, false);
       }
-
-      start = origin.copy();
-      start.translate(dir, 2);
-      end = start.copy();
-      start.translate(dir.antiClockwise());
-      end.translate(dir.clockwise());
-      RectSolid.newRect(start, end).fill(worldEditor, bars);
-
     }
+  }
+
+  private void generateCornerExternalRailings(Coord origin) {
+    for (Direction dir: Direction.CARDINAL) {
+      Coord start = origin.copy().translate(dir, 2).translate(dir.antiClockwise());
+      Coord end = origin.copy().translate(dir, 2).translate(dir.clockwise());
+      RectSolid railingCoords = RectSolid.newRect(start, end);
+      BlockType.IRON_BAR.getBrush().fill(worldEditor, railingCoords);
+    }
+  }
+
+  private void generateWalls(Coord origin) {
+    for (Direction dir : Direction.CARDINAL) {
+      Coord start = origin.copy().up(4).translate(dir, 3).translate(dir.antiClockwise(), 8);
+      Coord end = origin.copy().up(4).translate(dir, 3).translate(dir.clockwise(), 8);
+      RectSolid wall = RectSolid.newRect(start, end);
+      walls().fill(worldEditor, wall);
+
+      wall.translate(dir, 4);
+      walls().fill(worldEditor, wall);
+    }
+  }
+
+  private void generateWaterways(Coord origin, List<Direction> entrances) {
+    for (Direction dir : Direction.CARDINAL) {
+      if (!entrances.contains(dir)) {
+        generateWaterway(origin, dir);
+      }
+    }
+  }
+
+  private void generateWaterway(Coord origin, Direction dir) {
+    Coord start = origin.copy().translate(dir, 4).translate(dir.antiClockwise(), 3);
+    Coord end = origin.copy().translate(dir, 6).translate(dir.clockwise(), 3);
+
+    RectSolid waterway = RectSolid.newRect(start, end);
+    SingleBlockBrush.AIR.fill(worldEditor, waterway);
+
+    waterway.translate(Direction.DOWN, 1);
+    liquid().fill(worldEditor, waterway);
+
+    waterway.translate(Direction.DOWN, 1);
+    walls().fill(worldEditor, waterway);
+  }
+
+  private void generatePipes(Coord origin, List<Direction> entrances) {
+    for (Direction dir : Direction.CARDINAL) {
+      if (!entrances.contains(dir)) {
+        generatePipe(origin, dir);
+      }
+    }
+  }
+
+  private void generatePipe(Coord origin, Direction dir) {
+    Coord cursor = origin.copy();
+    cursor.translate(dir, 7);
+    walls().stroke(worldEditor, cursor);
+
+    cursor.up(2);
+    walls().stroke(worldEditor, cursor);
+
+    cursor.down();
+    cursor.translate(dir);
+    liquid().stroke(worldEditor, cursor);
+
+    generatePipeOpening(origin, dir);
+  }
+
+  private void generatePipeOpening(Coord origin, Direction dir) {
+    for (Direction o : dir.orthogonals()) {
+      Coord cursor = origin.copy();
+      cursor.translate(dir, 7);
+      cursor.translate(o);
+      stairs().setUpsideDown(true).setFacing(o).stroke(worldEditor, cursor);
+      cursor.up();
+      walls().stroke(worldEditor, cursor);
+      cursor.up();
+      stairs().setUpsideDown(false).setFacing(o).stroke(worldEditor, cursor);
+    }
+  }
+
+  private void generateRailings(Coord origin, List<Direction> entrances) {
+    for (Direction dir : Direction.CARDINAL) {
+      if (!entrances.contains(dir)) {
+        generateRailing(origin, dir);
+      }
+    }
+  }
+
+  private void generateRailing(Coord origin, Direction dir) {
+    Coord start = origin.copy().translate(dir, 3);
+    Coord end = start.copy();
+    start.translate(dir.antiClockwise(), 2);
+    end.translate(dir.clockwise(), 2);
+    BlockType.IRON_BAR.getBrush().fill(worldEditor, RectSolid.newRect(start, end));
   }
 
   public int getSize() {
