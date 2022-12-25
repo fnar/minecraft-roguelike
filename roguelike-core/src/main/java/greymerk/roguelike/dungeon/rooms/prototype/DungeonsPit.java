fnar/minecraft-roguelike

commit 0e8044aae31e5bc7d6e4296b4b473a9d6e6977ae

🧹 Remove method (Coord.chooseFrom(List<Coord>, Random)) used for selecting a chest location. - Rather than passing a list of coordinates, just randomly generate 1 coord

roguelike-core/src/main/java/greymerk/roguelike/dungeon/rooms/prototype/DungeonsPit.java
@@ -4,7 +4,6 @@ import com.github.fnar.minecraft.block.BlockType;
 import com.github.fnar.minecraft.block.SingleBlockBrush;
 import com.github.fnar.minecraft.block.decorative.TorchBlock;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import greymerk.roguelike.dungeon.base.BaseRoom;
@@ -17,9 +16,7 @@ import greymerk.roguelike.worldgen.WorldEditor;
 import greymerk.roguelike.worldgen.shapes.RectHollow;
 
 public class DungeonsPit extends BaseRoom {
-  int originX;
-  int originY;
-  int originZ;
+  private Coord origin;
   byte dungeonHeight;
   int dungeonLength;
   int dungeonWidth;
@@ -34,10 +31,9 @@ public class DungeonsPit extends BaseRoom {
     dungeonWidth = 2;
   }
 
+  @Override
   public BaseRoom generate(Coord origin, List<Direction> entrances) {
-    originX = origin.getX();
-    originY = origin.getY();
-    originZ = origin.getZ();
+    this.origin = origin;
 
     blocks = walls();
 
@@ -51,25 +47,23 @@ public class DungeonsPit extends BaseRoom {
       setTrap(dir, origin);
     }
 
-    List<Coord> spaces = new ArrayList<>();
-    spaces.add(new Coord(originX - 2, originY, originZ - 2));
-    spaces.add(new Coord(originX - 2, originY, originZ + 2));
-    spaces.add(new Coord(originX + 2, originY, originZ - 2));
-    spaces.add(new Coord(originX + 2, originY, originZ + 2));
+    Coord chestCoord = origin.copy()
+        .north(random().nextBoolean() ? 2 : -2)
+        .east(random().nextBoolean() ? 2 : -2);
 
-    generateTrappableChest(Coord.randomFrom(spaces, random()), getEntrance(entrances));
+    generateTrappableChest(chestCoord, getEntrance(entrances));
 
     return this;
   }
 
   protected void buildWalls() {
-    for (int blockX = originX - dungeonLength - 1; blockX <= originX + dungeonLength + 1; blockX++) {
-      for (int blockY = originY + dungeonHeight; blockY >= originY - 1; blockY--) {
-        for (int blockZ = originZ - dungeonWidth - 1; blockZ <= originZ + dungeonWidth + 1; blockZ++) {
-          if (blockX == originX - dungeonLength - 1
-              || blockZ == originZ - dungeonWidth - 1
-              || blockX == originX + dungeonLength + 1
-              || blockZ == originZ + dungeonWidth + 1) {
+    for (int blockX = origin.getX() - dungeonLength - 1; blockX <= origin.getX() + dungeonLength + 1; blockX++) {
+      for (int blockY = origin.getY() + dungeonHeight; blockY >= origin.getY() - 1; blockY--) {
+        for (int blockZ = origin.getY() - dungeonWidth - 1; blockZ <= origin.getY() + dungeonWidth + 1; blockZ++) {
+          if (blockX == origin.getX() - dungeonLength - 1
+              || blockZ == origin.getY() - dungeonWidth - 1
+              || blockX == origin.getX() + dungeonLength + 1
+              || blockZ == origin.getY() + dungeonWidth + 1) {
 
             if (blockY >= 0 && !worldEditor.isSolidBlock(new Coord(blockX, blockY - 1, blockZ))) {
               SingleBlockBrush.AIR.stroke(worldEditor, new Coord(blockX, blockY, blockZ));
@@ -92,16 +86,16 @@ public class DungeonsPit extends BaseRoom {
 
   protected void buildFloor() {
 
-    for (int blockX = originX - dungeonLength - 1; blockX <= originX + dungeonLength + 1; blockX++) {
-      for (int blockZ = originZ - dungeonWidth - 1; blockZ <= originZ + dungeonWidth + 1; blockZ++) {
-        blocks.stroke(worldEditor, new Coord(blockX, originY - 1, blockZ));
+    for (int blockX = origin.getX() - dungeonLength - 1; blockX <= origin.getX() + dungeonLength + 1; blockX++) {
+      for (int blockZ = origin.getY() - dungeonWidth - 1; blockZ <= origin.getY() + dungeonWidth + 1; blockZ++) {
+        blocks.stroke(worldEditor, new Coord(blockX, origin.getY() - 1, blockZ));
       }
     }
   }
 
   protected void buildRoof() {
-    for (int blockX = originX - dungeonLength - 1; blockX <= originX + dungeonLength + 1; blockX++) {
-      for (int blockZ = originZ - dungeonWidth - 1; blockZ <= originZ + dungeonWidth + 1; blockZ++) {
+    for (int blockX = origin.getX() - dungeonLength - 1; blockX <= origin.getX() + dungeonLength + 1; blockX++) {
+      for (int blockZ = origin.getY() - dungeonWidth - 1; blockZ <= origin.getY() + dungeonWidth + 1; blockZ++) {
         blocks.stroke(worldEditor, new Coord(blockX, dungeonHeight + 1, blockZ));
       }
     }
@@ -109,9 +103,9 @@ public class DungeonsPit extends BaseRoom {
 
   private void buildPit() {
 
-    for (int x = originX - 2; x <= originX + 2; x++) {
-      for (int z = originZ - 2; z <= originZ + 2; z++) {
-        for (int y = originY - 1; y > 0; y--) {
+    for (int x = origin.getX() - 2; x <= origin.getX() + 2; x++) {
+      for (int z = origin.getY() - 2; z <= origin.getY() + 2; z++) {
+        for (int y = origin.getY() - 1; y > 0; y--) {
 
           Coord pos = new Coord(x, y, z);
 
@@ -123,10 +117,10 @@ public class DungeonsPit extends BaseRoom {
             continue;
           }
 
-          if (x == originX - 2
-              || x == originX + 2
-              || z == originZ - 2
-              || z == originZ + 2) {
+          if (x == origin.getX() - 2
+              || x == origin.getX() + 2
+              || z == origin.getY() - 2
+              || z == origin.getY() + 2) {
 
             blocks.stroke(worldEditor, pos);
             continue;
@@ -147,14 +141,14 @@ public class DungeonsPit extends BaseRoom {
     BlockBrush plate = BlockType.PRESSURE_PLATE_STONE.getBrush();
     BlockBrush wire = BlockType.REDSTONE_WIRE.getBrush();
 
-    Coord start = origin.copy();
-    start.translate(dir, 3);
-    start.down();
-    start.translate(dir.antiClockwise());
-    Coord end = origin.copy();
-    end.translate(dir, 6);
-    end.up(3);
-    end.translate(dir.clockwise());
+    Coord start = origin.copy()
+        .translate(dir, 3)
+        .down()
+        .translate(dir.antiClockwise());
+    Coord end = origin.copy()
+        .translate(dir, 6)
+        .up(3)
+        .translate(dir.clockwise());
 
     for (Coord cell : new RectHollow(start, end)) {
       if (worldEditor.isAirBlock(cell)) {

roguelike-core/src/main/java/greymerk/roguelike/dungeon/base/BaseRoom.java
@@ -242,6 +242,14 @@ public abstract class BaseRoom implements Comparable<BaseRoom> {
     return entrances.get(0);
   }
 
+  protected Coord generateChestLocation(Coord origin) {
+    Direction dir0 = Direction.randomCardinal(random());
+    Direction dir1 = dir0.orthogonals()[random().nextBoolean() ? 0 : 1];
+    return origin.copy()
+        .translate(dir0, 3)
+        .translate(dir1, 2);
+  }
+
   public abstract int getSize();
 
   public boolean isValidLocation(Direction dir, Coord pos) {

roguelike-core/src/main/java/greymerk/roguelike/dungeon/rooms/prototype/BrickRoom.java
@@ -2,9 +2,7 @@ package greymerk.roguelike.dungeon.rooms.prototype;
 
 import com.github.fnar.minecraft.block.SingleBlockBrush;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import greymerk.roguelike.dungeon.base.BaseRoom;
 import greymerk.roguelike.dungeon.rooms.RoomSetting;
@@ -125,19 +123,9 @@ public class BrickRoom extends BaseRoom {
     boolean isChestAboveSpawner = random().nextInt(Math.max(1, getDifficulty(origin)) + 1) != 0;
     Coord chestLocation = isChestAboveSpawner
         ? spawnerLocation.copy().up()
-        : Coord.randomFrom(getPotentialChestLocations(origin), random());
+        : generateChestLocation(origin);
 
     generateTrappableChest(chestLocation, facing);
-  }
-
-  public List<Coord> getPotentialChestLocations(Coord origin) {
-    return CARDINAL.stream()
-        .flatMap(dir ->
-            Arrays.stream(dir.orthogonals())
-                .map(orthogonal -> origin.copy()
-                    .translate(dir, 3)
-                    .translate(orthogonal, 2)))
-        .collect(Collectors.toList());
   }
 
   public int getSize() {

roguelike-core/src/main/java/greymerk/roguelike/dungeon/rooms/prototype/CakeRoom.java
@@ -20,53 +20,57 @@ import greymerk.roguelike.worldgen.shapes.RectSolid;
 
 public class CakeRoom extends BaseRoom {
 
+  private int width;
+  private int length;
+
   public CakeRoom(RoomSetting roomSetting, LevelSettings levelSettings, WorldEditor worldEditor) {
     super(roomSetting, levelSettings, worldEditor);
   }
 
   @Override
   public BaseRoom generate(Coord origin, List<Direction> entrances) {
-    Random random = random();
     int x = origin.getX();
     int y = origin.getY();
     int z = origin.getZ();
-    final int HEIGHT = 3;
-    final int WIDTH = random.nextInt(2) + 2;
-    final int LENGTH = random.nextInt(2) + 3;
-
+    width = random().nextInt(2) + 2;
+    length = random().nextInt(2) + 3;
+    int height = 3;
 
     RectSolid.newRect(
-        new Coord(x - WIDTH, y, z - LENGTH),
-        new Coord(x + WIDTH, y + HEIGHT, z + LENGTH))
+        new Coord(x - width, y, z - length),
+        new Coord(x + width, y + height, z + length))
         .fill(worldEditor, SingleBlockBrush.AIR);
 
     RectHollow.newRect(
-        origin.copy().west(WIDTH + 1).north(LENGTH + 1).down(),
-        origin.copy().east(WIDTH + 1).south(LENGTH + 1).up(HEIGHT + 1)
+        origin.copy().west(width + 1).north(length + 1).down(),
+        origin.copy().east(width + 1).south(length + 1).up(height + 1)
     ).fill(worldEditor, floors(), false, true);
 
-    RectSolid.newRect(new Coord(x - WIDTH, y, z - LENGTH), new Coord(x - WIDTH, y + HEIGHT, z - LENGTH)).fill(worldEditor, pillars());
-    RectSolid.newRect(new Coord(x - WIDTH, y, z + LENGTH), new Coord(x - WIDTH, y + HEIGHT, z + LENGTH)).fill(worldEditor, pillars());
-    RectSolid.newRect(new Coord(x + WIDTH, y, z - LENGTH), new Coord(x + WIDTH, y + HEIGHT, z - LENGTH)).fill(worldEditor, pillars());
-    RectSolid.newRect(new Coord(x + WIDTH, y, z + LENGTH), new Coord(x + WIDTH, y + HEIGHT, z + LENGTH)).fill(worldEditor, pillars());
+    RectSolid.newRect(new Coord(x - width, y, z - length), new Coord(x - width, y + height, z - length)).fill(worldEditor, pillars());
+    RectSolid.newRect(new Coord(x - width, y, z + length), new Coord(x - width, y + height, z + length)).fill(worldEditor, pillars());
+    RectSolid.newRect(new Coord(x + width, y, z - length), new Coord(x + width, y + height, z - length)).fill(worldEditor, pillars());
+    RectSolid.newRect(new Coord(x + width, y, z + length), new Coord(x + width, y + height, z + length)).fill(worldEditor, pillars());
 
-    lights().stroke(worldEditor, new Coord(x - WIDTH + 1, y + HEIGHT + 1, z - LENGTH + 1));
-    lights().stroke(worldEditor, new Coord(x - WIDTH + 1, y + HEIGHT + 1, z + LENGTH - 1));
-    lights().stroke(worldEditor, new Coord(x + WIDTH - 1, y + HEIGHT + 1, z - LENGTH + 1));
-    lights().stroke(worldEditor, new Coord(x + WIDTH - 1, y + HEIGHT + 1, z + LENGTH - 1));
+    lights().stroke(worldEditor, new Coord(x - width + 1, y + height + 1, z - length + 1));
+    lights().stroke(worldEditor, new Coord(x - width + 1, y + height + 1, z + length - 1));
+    lights().stroke(worldEditor, new Coord(x + width - 1, y + height + 1, z - length + 1));
+    lights().stroke(worldEditor, new Coord(x + width - 1, y + height + 1, z + length - 1));
 
     placeCake(origin, pillars());
 
-    List<Coord> spaces = new ArrayList<>();
-    spaces.add(new Coord(x - WIDTH, y, z - LENGTH + 1));
-    spaces.add(new Coord(x - WIDTH, y, z + LENGTH - 1));
-    spaces.add(new Coord(x + WIDTH, y, z - LENGTH + 1));
-    spaces.add(new Coord(x + WIDTH, y, z + LENGTH - 1));
-
-    generateChest(Coord.randomFrom(spaces, random()), getEntrance(entrances).reverse(), ChestType.FOOD);
+    generateChest(generateChestLocation(origin), getEntrance(entrances).reverse(), ChestType.FOOD);
 
     generateDoorways(origin, entrances);
     return this;
+  }
+
+  @Override
+  protected Coord generateChestLocation(Coord origin) {
+    Direction dir0 = Direction.randomCardinal(random());
+    Direction dir1 = dir0.orthogonals()[random().nextBoolean() ? 0 : 1];
+    return origin.copy()
+        .translate(dir0, width)
+        .translate(dir1, length);
   }
 
   public void placeCake(Coord origin, BlockBrush pillar) {

roguelike-core/src/main/java/greymerk/roguelike/dungeon/rooms/prototype/DungeonsCreeperDen.java
@@ -40,28 +40,20 @@ public class DungeonsCreeperDen extends BaseRoom {
     subfloor.addBlock(floor, 3);
     subfloor.addBlock(TNT_META_BLOCK, 1);
 
-    Coord start = origin.copy();
-    Coord end = origin.copy();
-    start.translate(new Coord(-4, -4, -4));
-    end.translate(new Coord(4, 5, 4));
+    Coord start = origin.copy().translate(-4, -4, -4);
+    Coord end = origin.copy().translate(4, 5, 4);
     RectHollow.newRect(start, end).fill(worldEditor, mossy, false, true);
 
-    start = origin.copy();
-    end = origin.copy();
-    start.translate(new Coord(-3, -1, -3));
-    end.translate(new Coord(3, -1, 3));
+    start = origin.copy().translate(-3, -1, -3);
+    end = origin.copy().translate(3, -1, 3);
     RectSolid.newRect(start, end).fill(worldEditor, floor);
 
-    start = origin.copy();
-    end = origin.copy();
-    start.translate(new Coord(-3, -3, -3));
-    end.translate(new Coord(3, -2, 3));
+    start = origin.copy().translate(-3, -3, -3);
+    end = origin.copy().translate(3, -2, 3);
     RectSolid.newRect(start, end).fill(worldEditor, subfloor);
 
-    start = origin.copy();
-    end = origin.copy();
-    start.translate(new Coord(-3, 0, -3));
-    end.translate(new Coord(3, 0, 3));
+    start = origin.copy().translate(-3, 0, -3);
+    end = origin.copy().translate(3, 0, 3);
 
     List<Coord> chestSpaces = new RectSolid(start, end).get();
     Coord.randomFrom(chestSpaces, 3, random())

roguelike-core/src/main/java/greymerk/roguelike/dungeon/rooms/prototype/DungeonsEnchant.java
@@ -283,14 +283,6 @@ public class DungeonsEnchant extends BaseRoom {
       wall.stroke(worldEditor, cursor);
       cursor.translate(entrance);
       stair.setUpsideDown(true).setFacing(o.reverse()).stroke(worldEditor, cursor);
-
-      start = origin.copy();
-      start.up();
-      start.translate(o, 4);
-      end = start.copy();
-      start.translate(o.antiClockwise());
-      end.translate(o.clockwise());
-      chests.addAll(new RectSolid(start, end).get());
     }
 
     cursor = origin.copy();
@@ -304,7 +296,16 @@ public class DungeonsEnchant extends BaseRoom {
     cursor.translate(entrance, 5);
     BlockType.ENCHANTING_TABLE.getBrush().stroke(worldEditor, cursor);
 
-    generateChest(Coord.randomFrom(chests, random()), getEntrance(entrances), ChestType.ENCHANTING);
+    generateChest(generateChestLocation(origin.copy().up()), getEntrance(entrances), ChestType.ENCHANTING);
+  }
+
+  @Override
+  protected Coord generateChestLocation(Coord origin) {
+    Direction dir0 = Direction.randomCardinal(random());
+    Direction dir1 = dir0.orthogonals()[random().nextBoolean() ? 0 : 1];
+    return origin.copy()
+        .translate(dir0, 4)
+        .translate(dir1, 1);
   }
 
   @Override

roguelike-core/src/main/java/greymerk/roguelike/dungeon/rooms/prototype/DungeonsMusic.java
@@ -60,7 +60,6 @@ public class DungeonsMusic extends BaseRoom {
       RectSolid.newRect(start, end).fill(worldEditor, carpet);
     }
 
-    List<Coord> chests = new ArrayList<>();
     Coord cursor;
     for (Direction dir : Direction.CARDINAL) {
 
@@ -106,14 +105,6 @@ public class DungeonsMusic extends BaseRoom {
         cursor = origin.copy();
         cursor.translate(dir, 5);
         cursor.translate(o, 3);
-        cursor.up();
-        chests.add(cursor.copy());
-        cursor.translate(o);
-        chests.add(cursor.copy());
-
-        cursor = origin.copy();
-        cursor.translate(dir, 5);
-        cursor.translate(o, 3);
         secondaryStairs().setUpsideDown(true).setFacing(dir.reverse()).stroke(worldEditor, cursor);
         cursor.translate(o);
         secondaryStairs().setUpsideDown(true).setFacing(dir.reverse()).stroke(worldEditor, cursor);
@@ -139,11 +130,20 @@ public class DungeonsMusic extends BaseRoom {
     cursor.up(4);
     lights().stroke(worldEditor, cursor);
 
-    generateChest(Coord.randomFrom(chests, random()), getEntrance(entrances).reverse(), ChestType.MUSIC);
+    generateChest(generateChestLocation(origin.copy().up()), getEntrance(entrances).reverse(), ChestType.MUSIC);
 
     generateDoorways(origin, entrances);
 
     return this;
+  }
+
+  @Override
+  protected Coord generateChestLocation(Coord origin) {
+    Direction dir0 = Direction.randomCardinal(random());
+    Direction dir1 = dir0.orthogonals()[random().nextBoolean() ? 0 : 1];
+    return origin.copy()
+        .translate(dir0, 5)
+        .translate(dir1, 3 + (random().nextBoolean() ? 1 : 0));
   }
 
   private void pillar(Coord origin) {

roguelike-core/src/main/java/greymerk/roguelike/dungeon/rooms/prototype/DungeonsNetherBrick.java
@@ -36,7 +36,7 @@ public class DungeonsNetherBrick extends BaseRoom {
     generateWalls(x, y, z, length, width);
     generateFloor(origin, x, y, z, length, width);
     generateCeiling(x, y, z, length, width);
-    generateChests(entrances, x, y, z, length, width);
+    generateTrappableChest(generateChestLocation(origin), getEntrance(entrances));
     generateSpawners(origin, random, length, width);
 
     return this;
@@ -74,9 +74,13 @@ public class DungeonsNetherBrick extends BaseRoom {
     ).fill(worldEditor, subFloor);
   }
 
-  private void generateChests(List<Direction> entrances, int x, int y, int z, int length, int width) {
-    Coord chestLocation = Coord.randomFrom(new RectSolid(new Coord(x - length, y, z - width), new Coord(x + length, y, z + width)).get(), random());
-    generateTrappableChest(chestLocation, getEntrance(entrances));
+  @Override
+  protected Coord generateChestLocation(Coord origin) {
+    Direction dir0 = Direction.randomCardinal(random());
+    Direction dir1 = dir0.orthogonals()[random().nextBoolean() ? 0 : 1];
+    return origin.copy()
+        .translate(dir0, 3)
+        .translate(dir1, 2);
   }
 
   private void generateSpawners(Coord origin, Random random, int length, int width) {

commit d4b8c204cff1e8f8d05df96576e9682d8fb999fa

🐛 Fix Pit room - bug introduced in 0e8044aae31e5bc7d6e4296b4b473a9d6e6977ae by accidentally using origin.getY() instead of origin.getZ()

roguelike-core/src/main/java/greymerk/roguelike/dungeon/rooms/prototype/DungeonsPit.java
@@ -16,19 +16,17 @@ import greymerk.roguelike.worldgen.WorldEditor;
 import greymerk.roguelike.worldgen.shapes.RectHollow;
 
 public class DungeonsPit extends BaseRoom {
+
   private Coord origin;
-  byte dungeonHeight;
-  int dungeonLength;
-  int dungeonWidth;
+  byte dungeonHeight = 3;
+  int dungeonLength = 2;
+  int dungeonWidth = 2;
   int woolColor;
   int numChests;
   BlockBrush blocks;
 
   public DungeonsPit(RoomSetting roomSetting, LevelSettings levelSettings, WorldEditor worldEditor) {
     super(roomSetting, levelSettings, worldEditor);
-    dungeonHeight = 3;
-    dungeonLength = 2;
-    dungeonWidth = 2;
   }
 
   @Override
@@ -41,7 +39,6 @@ public class DungeonsPit extends BaseRoom {
     buildFloor();
     buildRoof();
     buildPit();
-
 
     for (Direction dir : Direction.CARDINAL) {
       setTrap(dir, origin);
@@ -59,11 +56,11 @@ public class DungeonsPit extends BaseRoom {
   protected void buildWalls() {
     for (int blockX = origin.getX() - dungeonLength - 1; blockX <= origin.getX() + dungeonLength + 1; blockX++) {
       for (int blockY = origin.getY() + dungeonHeight; blockY >= origin.getY() - 1; blockY--) {
-        for (int blockZ = origin.getY() - dungeonWidth - 1; blockZ <= origin.getY() + dungeonWidth + 1; blockZ++) {
+        for (int blockZ = origin.getZ() - dungeonWidth - 1; blockZ <= origin.getZ() + dungeonWidth + 1; blockZ++) {
           if (blockX == origin.getX() - dungeonLength - 1
-              || blockZ == origin.getY() - dungeonWidth - 1
+              || blockZ == origin.getZ() - dungeonWidth - 1
               || blockX == origin.getX() + dungeonLength + 1
-              || blockZ == origin.getY() + dungeonWidth + 1) {
+              || blockZ == origin.getZ() + dungeonWidth + 1) {
 
             if (blockY >= 0 && !worldEditor.isSolidBlock(new Coord(blockX, blockY - 1, blockZ))) {
               SingleBlockBrush.AIR.stroke(worldEditor, new Coord(blockX, blockY, blockZ));
@@ -87,7 +84,7 @@ public class DungeonsPit extends BaseRoom {
   protected void buildFloor() {
 
     for (int blockX = origin.getX() - dungeonLength - 1; blockX <= origin.getX() + dungeonLength + 1; blockX++) {
-      for (int blockZ = origin.getY() - dungeonWidth - 1; blockZ <= origin.getY() + dungeonWidth + 1; blockZ++) {
+      for (int blockZ = origin.getZ() - dungeonWidth - 1; blockZ <= origin.getZ() + dungeonWidth + 1; blockZ++) {
         blocks.stroke(worldEditor, new Coord(blockX, origin.getY() - 1, blockZ));
       }
     }
@@ -95,7 +92,7 @@ public class DungeonsPit extends BaseRoom {
 
   protected void buildRoof() {
     for (int blockX = origin.getX() - dungeonLength - 1; blockX <= origin.getX() + dungeonLength + 1; blockX++) {
-      for (int blockZ = origin.getY() - dungeonWidth - 1; blockZ <= origin.getY() + dungeonWidth + 1; blockZ++) {
+      for (int blockZ = origin.getZ() - dungeonWidth - 1; blockZ <= origin.getZ() + dungeonWidth + 1; blockZ++) {
         blocks.stroke(worldEditor, new Coord(blockX, dungeonHeight + 1, blockZ));
       }
     }
@@ -104,7 +101,7 @@ public class DungeonsPit extends BaseRoom {
   private void buildPit() {
 
     for (int x = origin.getX() - 2; x <= origin.getX() + 2; x++) {
-      for (int z = origin.getY() - 2; z <= origin.getY() + 2; z++) {
+      for (int z = origin.getZ() - 2; z <= origin.getZ() + 2; z++) {
         for (int y = origin.getY() - 1; y > 0; y--) {
 
           Coord pos = new Coord(x, y, z);
@@ -119,8 +116,8 @@ public class DungeonsPit extends BaseRoom {
 
           if (x == origin.getX() - 2
               || x == origin.getX() + 2
-              || z == origin.getY() - 2
-              || z == origin.getY() + 2) {
+              || z == origin.getZ() - 2
+              || z == origin.getZ() + 2) {
 
             blocks.stroke(worldEditor, pos);
             continue;