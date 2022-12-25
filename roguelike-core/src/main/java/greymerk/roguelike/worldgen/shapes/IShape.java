c344f40b529aeb5941f02eb8af34ef0f757ba323
Fnar <fnardottv@gmail.com>
2022-12-25 15:43:30 -0800

    🔨 Add convenience translation functions to IShape - I am certainly going to regret this. Shapes shouldn't care about directions. Instead, shapes should produce coord vectors, that can be translated by a given point, and subsequent translations should be able to be layered on top. Ahh, one day.

--- roguelike-core/src/main/java/greymerk/roguelike/worldgen/shapes/IShape.java
+++ roguelike-core/src/main/java/greymerk/roguelike/worldgen/shapes/IShape.java
@@ -32,6 +32,59 @@ public interface IShape extends Iterable<Coord> {
     return this;
   }
 
+  default IShape translate(Direction direction) {
+    translate(direction, 1);
+    return this;
+  }
+
+  default IShape up(int amount) {
+    return translate(Direction.UP, amount);
+  }
+
+  default IShape up() {
+    return up(1);
+  }
+
+  default IShape down(int amount) {
+    return translate(Direction.DOWN, amount);
+  }
+
+  default IShape down() {
+    return down(1);
+  }
+
+  default IShape north(int amount) {
+    return translate(Direction.NORTH, amount);
+  }
+
+  default IShape north() {
+    return north(1);
+  }
+
+  default IShape east(int amount) {
+    return translate(Direction.EAST, amount);
+  }
+
+  default IShape east() {
+    return east(1);
+  }
+
+  default IShape south(int amount) {
+    return translate(Direction.SOUTH, amount);
+  }
+
+  default IShape south() {
+    return south(1);
+  }
+
+  default IShape west(int amount) {
+    return translate(Direction.WEST, amount);
+  }
+
+  default IShape west() {
+    return west(1);
+  }
+
   default List<Coord> asList() {
     List<Coord> coords = Lists.newArrayList();
     for(Coord c : this) {
