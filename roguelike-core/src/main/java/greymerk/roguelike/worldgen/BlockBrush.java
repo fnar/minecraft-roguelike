9ba68be720dd3d87cdc73305c4a055c5171fdeaf
Fnar <fnardottv@gmail.com>
2022-12-24 22:47:34 -0800

    🔨 Add utility function, BlockBrush.fill(WorldEditor, Collection<Coord>)

--- roguelike-core/src/main/java/greymerk/roguelike/worldgen/BlockBrush.java
+++ roguelike-core/src/main/java/greymerk/roguelike/worldgen/BlockBrush.java
@@ -1,24 +1,39 @@
 package greymerk.roguelike.worldgen;
 
+import java.util.Collection;
+
 import greymerk.roguelike.worldgen.shapes.IShape;
 
 public interface BlockBrush {
 
+  boolean FILL_AIR_DEFAULT = true;
+  boolean REPLACE_SOLID_DEFAULT = true;
+
   boolean stroke(WorldEditor editor, Coord pos, boolean fillAir, boolean replaceSolid);
 
   default boolean stroke(WorldEditor editor, Coord pos) {
-    return stroke(editor, pos, true, true);
+    return stroke(editor, pos, FILL_AIR_DEFAULT, REPLACE_SOLID_DEFAULT);
   }
 
   default IShape fill(WorldEditor editor, IShape shape) {
-    return fill(editor, shape, true, true);
+    return fill(editor, shape, FILL_AIR_DEFAULT, REPLACE_SOLID_DEFAULT);
   }
 
   default IShape fill(WorldEditor editor, IShape shape, boolean fillAir, boolean replaceSolid) {
     return shape.fill(editor, this, fillAir, replaceSolid);
   }
 
-  default BlockBrush setFacing(Direction direction) { return this; }
+  default void fill(WorldEditor editor, Collection<Coord> coords, boolean fillAir, boolean replaceSolid) {
+    coords.forEach(coord -> stroke(editor, coord, fillAir, replaceSolid));
+  }
+
+  default void fill(WorldEditor editor, Collection<Coord> coords) {
+    fill(editor, coords, FILL_AIR_DEFAULT, REPLACE_SOLID_DEFAULT);
+  }
+
+  default BlockBrush setFacing(Direction direction) {
+    return this;
+  }
 
   BlockBrush copy();
 
