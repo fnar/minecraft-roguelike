b092b84544324e9c73543eac0483088da297eaea
Fnar <fnardottv@gmail.com>
2023-01-06 03:43:29 -0800

    🔨 Begin reworking NetherBrick room to break dependency on getHeight and room bounding boxes

--- roguelike-core/src/main/java/greymerk/roguelike/dungeon/rooms/prototype/DungeonsNetherBrick.java
+++ roguelike-core/src/main/java/greymerk/roguelike/dungeon/rooms/prototype/DungeonsNetherBrick.java
@@ -4,96 +4,72 @@ import com.github.fnar.minecraft.block.BlockType;
 import com.github.fnar.minecraft.block.SingleBlockBrush;
 
 import java.util.List;
-import java.util.Random;
 
 import greymerk.roguelike.dungeon.base.BaseRoom;
 import greymerk.roguelike.dungeon.rooms.RoomSetting;
 import greymerk.roguelike.dungeon.settings.LevelSettings;
-import greymerk.roguelike.worldgen.BlockBrush;
 import greymerk.roguelike.worldgen.BlockWeightedRandom;
 import greymerk.roguelike.worldgen.Coord;
 import greymerk.roguelike.worldgen.Direction;
 import greymerk.roguelike.worldgen.WorldEditor;
-import greymerk.roguelike.worldgen.shapes.RectHollow;
-import greymerk.roguelike.worldgen.shapes.RectSolid;
+import greymerk.roguelike.worldgen.shapes.IShape;
 
 public class DungeonsNetherBrick extends BaseRoom {
 
   public DungeonsNetherBrick(RoomSetting roomSetting, LevelSettings levelSettings, WorldEditor worldEditor) {
     super(roomSetting, levelSettings, worldEditor);
-    this.height = 3;
+    this.size = 6;
+    this.height = 4;
   }
 
-  public BaseRoom generate(Coord origin, List<Direction> entrances) {
-    Random random = random();
-
-    int x = origin.getX();
-    int y = origin.getY();
-    int z = origin.getZ();
-
-    int length = 2 + random.nextInt(3);
-    int width = 2 + random.nextInt(3);
-
-    generateWalls(x, y, z, length, width);
-    generateFloor(origin, x, y, z, length, width);
-    generateCeiling(x, y, z, length, width);
-    generateTrappableChest(generateChestLocation(origin), getEntrance(entrances));
-    generateSpawners(origin, random, length, width);
-    generateDoorways(origin, entrances);
+  public BaseRoom generate(Coord at, List<Direction> entrances) {
+    generateWalls(at);
+    generateFloor(at);
+    generateCeiling(at);
+    generateTrappableChest(generateChestLocation(at), getEntrance(entrances));
+    generateSpawners(at);
+    generateDoorways(at, entrances);
 
     return this;
   }
 
-  private void generateWalls(int x, int y, int z, int length, int width) {
-    RectHollow.newRect(new Coord(x - length - 1, y - 1, z - width - 1), new Coord(x + length + 1, y + getHeight() + 1, z + width + 1)).fill(worldEditor, walls(), false, true);
+  private void generateWalls(Coord at) {
+    IShape wallsRect = at.newHollowRect(getWallDist()).withHeight(getCeilingHeight() + 1 + getDepth() + 1).down(getDepth());
+    walls().fill(worldEditor, wallsRect, false, true);
   }
 
-  private void generateFloor(Coord origin, int x, int y, int z, int length, int width) {
-    BlockBrush floor = floors();
-    floor.fill(worldEditor, RectSolid.newRect(new Coord(x - length - 1, y - 1, z - width - 1), new Coord(x + length + 1, y - 1, z + width + 1)));
-    generateCrapUnderneath(origin, length, width);
+  private void generateFloor(Coord at) {
+    IShape floorRect = at.newRect(getWallDist()).down(depth);
+    floors().fill(worldEditor, floorRect);
   }
 
-  private void generateCeiling(int x, int y, int z, int length, int width) {
-    BlockWeightedRandom ceiling = new BlockWeightedRandom();
-    ceiling.addBlock(BlockType.FENCE_NETHER_BRICK.getBrush(), 10);
-    ceiling.addBlock(SingleBlockBrush.AIR, 5);
-    ceiling.fill(worldEditor, RectSolid.newRect(new Coord(x - length, y + getHeight(), z - width), new Coord(x + length, y + getHeight(), z + width)));
+  private void generateCeiling(Coord at) {
+    IShape ceilingRect = at.newRect(getWallDist()).up(getCeilingHeight());
+    ceilingBrush().fill(worldEditor, ceilingRect);
   }
 
-  private void generateCrapUnderneath(Coord origin, int length, int width) {
-    // liquid crap under the floor
-    BlockWeightedRandom subFloor = new BlockWeightedRandom();
-    subFloor.addBlock(liquid(), 8);
-    subFloor.addBlock(BlockType.OBSIDIAN.getBrush(), 3);
-    RectSolid rect = RectSolid.newRect(
-        origin.copy().translate(-length, -5, -width),
-        origin.copy().translate(length, -2, width)
-    );
-    subFloor.fill(worldEditor, rect);
+  private BlockWeightedRandom ceilingBrush() {
+    BlockWeightedRandom ceilingBrush = new BlockWeightedRandom();
+    ceilingBrush.addBlock(BlockType.FENCE_NETHER_BRICK.getBrush(), 10);
+    ceilingBrush.addBlock(SingleBlockBrush.AIR, 5);
+    return ceilingBrush;
   }
 
   @Override
-  protected Coord generateChestLocation(Coord origin) {
+  protected Coord generateChestLocation(Coord at) {
     Direction dir0 = Direction.randomCardinal(random());
     Direction dir1 = dir0.orthogonals()[random().nextBoolean() ? 0 : 1];
-    return origin.copy()
+    return at.copy()
         .translate(dir0, 3)
         .translate(dir1, 2);
   }
 
-  private void generateSpawners(Coord origin, Random random, int length, int width) {
-    int negX = -length - 1;
-    int posX = length + 1;
-    int negZ = -width - 1;
-    int posZ = width + 1;
-    generateSpawner(origin.copy().translate(negX, random.nextInt(2), negZ));
-    generateSpawner(origin.copy().translate(negX, random.nextInt(2), posZ));
-    generateSpawner(origin.copy().translate(posX, random.nextInt(2), negZ));
-    generateSpawner(origin.copy().translate(posX, random.nextInt(2), posZ));
+  private void generateSpawners(Coord at) {
+    int dist = getWallDist();
+    for (Direction card : Direction.cardinals()) {
+      Coord spawnerCoord = at.copy().translate(card, dist).translate(card.left(), dist).up(random().nextInt(2));
+      generateSpawner(spawnerCoord);
+    }
   }
 
-  public int getSize() {
-    return 6;
-  }
 }
