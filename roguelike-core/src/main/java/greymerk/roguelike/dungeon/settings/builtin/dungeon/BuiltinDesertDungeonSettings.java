fnar/minecraft-roguelike

commit 71360ae2a0d72ad4e78650d90b79ff7b34ef511a

Liberate SpawnCriteria from dependence on net.minecraft

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinDesertDungeonSettings.java
@@ -1,6 +1,9 @@
 package greymerk.roguelike.dungeon.settings.builtin.dungeon;
 
 import com.github.fnar.minecraft.item.Material;
+import com.github.fnar.util.Strings;
+
+import java.util.Collections;
 
 import greymerk.roguelike.dungeon.layout.LayoutGenerator;
 import greymerk.roguelike.dungeon.base.RoomType;
@@ -30,7 +33,7 @@ public class BuiltinDesertDungeonSettings extends DungeonSettings {
     super(ID);
     setExclusive(true);
     getInherit().add(BuiltinBaseSettings.ID);
-    getCriteria().setBiomeTypes(newArrayList(SANDY));
+    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(SANDY))));
     setTowerSettings(new TowerSettings(TowerType.PYRAMID, Themes.PYRAMID));
     for (int i = 0; i < 5; ++i) {
       getLootRules().add(new SingleUseLootRule(new MinecraftItemLootItem(Material.Type.GOLD_INGOT.asItem(), 0, 1, 1 + i, 1), i, 6));

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinForestDungeonSettings.java
@@ -1,5 +1,9 @@
 package greymerk.roguelike.dungeon.settings.builtin.dungeon;
 
+import com.github.fnar.util.Strings;
+
+import java.util.Collections;
+
 import greymerk.roguelike.dungeon.base.RoomType;
 import greymerk.roguelike.dungeon.base.RoomsSetting;
 import greymerk.roguelike.dungeon.base.SecretsSetting;
@@ -24,7 +28,7 @@ public class BuiltinForestDungeonSettings {
     DungeonSettings dungeonSettings = new DungeonSettings().withId(new SettingIdentifier(SettingsContainer.BUILTIN_NAMESPACE, "forest"));
     dungeonSettings.setExclusive(true);
     dungeonSettings.getInherit().add(BuiltinBaseSettings.ID);
-    dungeonSettings.getCriteria().setBiomeTypes(newArrayList(FOREST));
+    dungeonSettings.getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(FOREST))));
     dungeonSettings.setTowerSettings(new TowerSettings(TowerType.ROGUE, Themes.TOWER));
 
     level0(dungeonSettings);

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinIceDungeonSettings.java
@@ -1,5 +1,8 @@
 package greymerk.roguelike.dungeon.settings.builtin.dungeon;
 
+import com.github.fnar.util.Strings;
+
+import java.util.Collections;
 import java.util.stream.IntStream;
 
 import greymerk.roguelike.dungeon.settings.DungeonSettings;
@@ -21,7 +24,7 @@ public class BuiltinIceDungeonSettings extends DungeonSettings {
     super(ID);
     setExclusive(true);
     getInherit().add(BuiltinBaseSettings.ID);
-    getCriteria().setBiomeTypes(newArrayList(SNOWY));
+    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(SNOWY))));
     setTowerSettings(new TowerSettings(PYRAMID, ICE));
     IntStream.range(0, MAXIMUM_COUNT_OF_LEVELS)
         .mapToObj(this::getLevelSettings)

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinJungleDungeonSettings.java
@@ -1,6 +1,9 @@
 package greymerk.roguelike.dungeon.settings.builtin.dungeon;
 
 import com.github.fnar.minecraft.item.Material;
+import com.github.fnar.util.Strings;
+
+import java.util.Collections;
 
 import greymerk.roguelike.dungeon.segment.Segment;
 import greymerk.roguelike.dungeon.segment.SegmentGenerator;
@@ -28,7 +31,7 @@ public class BuiltinJungleDungeonSettings extends DungeonSettings {
     super(ID);
     setExclusive(true);
     getInherit().add(BuiltinBaseSettings.ID);
-    getCriteria().setBiomeTypes(newArrayList(JUNGLE));
+    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(JUNGLE))));
     setTowerSettings(new TowerSettings(TowerType.JUNGLE, Themes.JUNGLE));
 
     for (int i = 0; i < 5; ++i) {

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinMesaDungeonSettings.java
@@ -1,5 +1,8 @@
 package greymerk.roguelike.dungeon.settings.builtin.dungeon;
 
+import com.github.fnar.util.Strings;
+
+import java.util.Collections;
 import java.util.stream.IntStream;
 
 import greymerk.roguelike.dungeon.settings.DungeonSettings;
@@ -21,7 +24,7 @@ public class BuiltinMesaDungeonSettings extends DungeonSettings {
     super(ID);
     setExclusive(true);
     getInherit().add(BuiltinBaseSettings.ID);
-    getCriteria().setBiomeTypes(newArrayList(MESA));
+    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(MESA))));
     setTowerSettings(new TowerSettings(TowerType.ETHO, Themes.ETHOTOWER));
 
     Themes[] themes = {Themes.ETHOTOWER, Themes.ETHOTOWER, Themes.CRYPT, Themes.CRYPT, Themes.NETHER};

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinMountainDungeonSettings.java
@@ -1,5 +1,9 @@
 package greymerk.roguelike.dungeon.settings.builtin.dungeon;
 
+import com.github.fnar.util.Strings;
+
+import java.util.Collections;
+
 import greymerk.roguelike.dungeon.base.RoomType;
 import greymerk.roguelike.dungeon.base.RoomsSetting;
 import greymerk.roguelike.dungeon.base.SecretsSetting;
@@ -26,7 +30,7 @@ public class BuiltinMountainDungeonSettings extends DungeonSettings {
     super(ID);
     setExclusive(true);
     getInherit().add(BuiltinBaseSettings.ID);
-    getCriteria().setBiomeTypes(newArrayList(MOUNTAIN));
+    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(MOUNTAIN))));
     setTowerSettings(new TowerSettings(TowerType.ENIKO, Themes.OAK));
 
     Themes[] themes = {Themes.ENIKO, Themes.ENIKO2, Themes.SEWER, Themes.MOSSY, Themes.NETHER};

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinPlainsDungeonSettings.java
@@ -1,5 +1,9 @@
 package greymerk.roguelike.dungeon.settings.builtin.dungeon;
 
+import com.github.fnar.util.Strings;
+
+import java.util.Collections;
+
 import greymerk.roguelike.dungeon.base.RoomType;
 import greymerk.roguelike.dungeon.base.RoomsSetting;
 import greymerk.roguelike.dungeon.base.SecretsSetting;
@@ -25,7 +29,7 @@ public class BuiltinPlainsDungeonSettings extends DungeonSettings {
     super(ID);
     setExclusive(true);
     getInherit().add(BuiltinBaseSettings.ID);
-    getCriteria().setBiomeTypes(newArrayList(PLAINS));
+    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(PLAINS))));
 
     setTowerSettings(new TowerSettings(TowerType.HOUSE, Themes.HOUSE));
 

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinSwampDungeonSettings.java
@@ -3,6 +3,9 @@ package greymerk.roguelike.dungeon.settings.builtin.dungeon;
 import com.github.fnar.minecraft.item.Ingredient;
 import com.github.fnar.minecraft.item.Material;
 import com.github.fnar.minecraft.item.RldItemStack;
+import com.github.fnar.util.Strings;
+
+import java.util.Collections;
 
 import greymerk.roguelike.dungeon.base.RoomType;
 import greymerk.roguelike.dungeon.base.RoomsSetting;
@@ -38,7 +41,7 @@ public class BuiltinSwampDungeonSettings extends DungeonSettings {
     super(ID);
     setExclusive(true);
     getInherit().add(BuiltinBaseSettings.ID);
-    getCriteria().setBiomeTypes(newArrayList(SWAMP));
+    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(SWAMP))));
     setTowerSettings(new TowerSettings(WITCH, DARKOAK));
 
     Themes[] themes = {DARKHALL, DARKHALL, MUDDY, MOSSY, NETHER};

commit 12f8d0f7da0cd9048bab83abd11417c02725898b

Fix issue introduced in 71360ae2a0d72ad4e78650d90b79ff7b34ef511a ("Liberate ...") where biome spawn criteria for builtin dungeons were wrapped in extraneous brackets (e.g. "[sandy]")

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinDesertDungeonSettings.java
@@ -5,10 +5,10 @@ import com.github.fnar.util.Strings;
 
 import java.util.Collections;
 
-import greymerk.roguelike.dungeon.layout.LayoutGenerator;
 import greymerk.roguelike.dungeon.base.RoomType;
 import greymerk.roguelike.dungeon.base.RoomsSetting;
 import greymerk.roguelike.dungeon.base.SecretsSetting;
+import greymerk.roguelike.dungeon.layout.LayoutGenerator;
 import greymerk.roguelike.dungeon.segment.Segment;
 import greymerk.roguelike.dungeon.segment.SegmentGenerator;
 import greymerk.roguelike.dungeon.settings.DungeonSettings;
@@ -22,7 +22,6 @@ import greymerk.roguelike.theme.Themes;
 import greymerk.roguelike.treasure.loot.MinecraftItemLootItem;
 import greymerk.roguelike.treasure.loot.rule.SingleUseLootRule;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static net.minecraftforge.common.BiomeDictionary.Type.SANDY;
 
 public class BuiltinDesertDungeonSettings extends DungeonSettings {
@@ -33,7 +32,7 @@ public class BuiltinDesertDungeonSettings extends DungeonSettings {
     super(ID);
     setExclusive(true);
     getInherit().add(BuiltinBaseSettings.ID);
-    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(SANDY))));
+    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(SANDY)));
     setTowerSettings(new TowerSettings(TowerType.PYRAMID, Themes.PYRAMID));
     for (int i = 0; i < 5; ++i) {
       getLootRules().add(new SingleUseLootRule(new MinecraftItemLootItem(Material.Type.GOLD_INGOT.asItem(), 0, 1, 1 + i, 1), i, 6));

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinForestDungeonSettings.java
@@ -19,7 +19,6 @@ import greymerk.roguelike.dungeon.towers.TowerType;
 import greymerk.roguelike.theme.Themes;
 import greymerk.roguelike.worldgen.filter.Filter;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static net.minecraftforge.common.BiomeDictionary.Type.FOREST;
 
 public class BuiltinForestDungeonSettings {
@@ -28,7 +27,7 @@ public class BuiltinForestDungeonSettings {
     DungeonSettings dungeonSettings = new DungeonSettings().withId(new SettingIdentifier(SettingsContainer.BUILTIN_NAMESPACE, "forest"));
     dungeonSettings.setExclusive(true);
     dungeonSettings.getInherit().add(BuiltinBaseSettings.ID);
-    dungeonSettings.getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(FOREST))));
+    dungeonSettings.getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(FOREST)));
     dungeonSettings.setTowerSettings(new TowerSettings(TowerType.ROGUE, Themes.TOWER));
 
     level0(dungeonSettings);

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinIceDungeonSettings.java
@@ -11,7 +11,6 @@ import greymerk.roguelike.dungeon.settings.SettingsContainer;
 import greymerk.roguelike.dungeon.settings.TowerSettings;
 import greymerk.roguelike.dungeon.settings.builtin.BuiltinBaseSettings;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static greymerk.roguelike.dungeon.towers.TowerType.PYRAMID;
 import static greymerk.roguelike.theme.Themes.ICE;
 import static net.minecraftforge.common.BiomeDictionary.Type.SNOWY;
@@ -24,7 +23,7 @@ public class BuiltinIceDungeonSettings extends DungeonSettings {
     super(ID);
     setExclusive(true);
     getInherit().add(BuiltinBaseSettings.ID);
-    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(SNOWY))));
+    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(SNOWY)));
     setTowerSettings(new TowerSettings(PYRAMID, ICE));
     IntStream.range(0, MAXIMUM_COUNT_OF_LEVELS)
             .mapToObj(this::getLevelSettings)

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinJungleDungeonSettings.java
@@ -20,7 +20,6 @@ import greymerk.roguelike.treasure.loot.rule.SingleUseLootRule;
 import greymerk.roguelike.util.WeightedChoice;
 import greymerk.roguelike.worldgen.filter.Filter;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static net.minecraftforge.common.BiomeDictionary.Type.JUNGLE;
 
 public class BuiltinJungleDungeonSettings extends DungeonSettings {
@@ -31,7 +30,7 @@ public class BuiltinJungleDungeonSettings extends DungeonSettings {
     super(ID);
     setExclusive(true);
     getInherit().add(BuiltinBaseSettings.ID);
-    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(JUNGLE))));
+    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(JUNGLE)));
     setTowerSettings(new TowerSettings(TowerType.JUNGLE, Themes.JUNGLE));
 
     for (int i = 0; i < 5; ++i) {

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinMesaDungeonSettings.java
@@ -13,7 +13,6 @@ import greymerk.roguelike.dungeon.settings.builtin.BuiltinBaseSettings;
 import greymerk.roguelike.dungeon.towers.TowerType;
 import greymerk.roguelike.theme.Themes;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static net.minecraftforge.common.BiomeDictionary.Type.MESA;
 
 public class BuiltinMesaDungeonSettings extends DungeonSettings {
@@ -24,7 +23,7 @@ public class BuiltinMesaDungeonSettings extends DungeonSettings {
     super(ID);
     setExclusive(true);
     getInherit().add(BuiltinBaseSettings.ID);
-    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(MESA))));
+    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(MESA)));
     setTowerSettings(new TowerSettings(TowerType.ETHO, Themes.ETHOTOWER));
 
     Themes[] themes = {Themes.ETHOTOWER, Themes.ETHOTOWER, Themes.CRYPT, Themes.CRYPT, Themes.NETHER};

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinMountainDungeonSettings.java
@@ -19,7 +19,6 @@ import greymerk.roguelike.dungeon.towers.TowerType;
 import greymerk.roguelike.theme.Themes;
 import greymerk.roguelike.worldgen.filter.Filter;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static net.minecraftforge.common.BiomeDictionary.Type.MOUNTAIN;
 
 public class BuiltinMountainDungeonSettings extends DungeonSettings {
@@ -30,7 +29,7 @@ public class BuiltinMountainDungeonSettings extends DungeonSettings {
     super(ID);
     setExclusive(true);
     getInherit().add(BuiltinBaseSettings.ID);
-    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(MOUNTAIN))));
+    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(MOUNTAIN)));
     setTowerSettings(new TowerSettings(TowerType.ENIKO, Themes.OAK));
 
     Themes[] themes = {Themes.ENIKO, Themes.ENIKO2, Themes.SEWER, Themes.MOSSY, Themes.NETHER};

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinPlainsDungeonSettings.java
@@ -17,7 +17,6 @@ import greymerk.roguelike.dungeon.towers.TowerType;
 import greymerk.roguelike.theme.Themes;
 import greymerk.roguelike.worldgen.filter.Filter;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static net.minecraftforge.common.BiomeDictionary.Type.PLAINS;
 
 public class BuiltinPlainsDungeonSettings extends DungeonSettings {
@@ -29,7 +28,7 @@ public class BuiltinPlainsDungeonSettings extends DungeonSettings {
     super(ID);
     setExclusive(true);
     getInherit().add(BuiltinBaseSettings.ID);
-    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(PLAINS))));
+    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(PLAINS)));
 
     setTowerSettings(new TowerSettings(TowerType.HOUSE, Themes.HOUSE));
 

roguelike-core/src/main/java/greymerk/roguelike/dungeon/settings/builtin/dungeon/BuiltinSwampDungeonSettings.java
@@ -24,7 +24,6 @@ import greymerk.roguelike.treasure.loot.rule.SingleUseLootRule;
 import greymerk.roguelike.util.WeightedRandomizer;
 import greymerk.roguelike.worldgen.filter.Filter;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static greymerk.roguelike.dungeon.towers.TowerType.WITCH;
 import static greymerk.roguelike.theme.Themes.DARKHALL;
 import static greymerk.roguelike.theme.Themes.DARKOAK;
@@ -41,7 +40,7 @@ public class BuiltinSwampDungeonSettings extends DungeonSettings {
     super(ID);
     setExclusive(true);
     getInherit().add(BuiltinBaseSettings.ID);
-    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(newArrayList(SWAMP))));
+    getCriteria().setBiomeTypes(Strings.asStrings(Collections.singleton(SWAMP)));
     setTowerSettings(new TowerSettings(WITCH, DARKOAK));
 
     Themes[] themes = {DARKHALL, DARKHALL, MUDDY, MOSSY, NETHER};