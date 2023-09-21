48bc1e4422c9c867c8d2859587b1e75bc39cae21
Fnar <fnardottv@gmail.com>
2023-09-20 23:45:13 -0700

    Move argument parsing to ArgumentParser

--- roguelike-core/src/main/java/greymerk/roguelike/util/ArgumentParser.java
+++ roguelike-core/src/main/java/greymerk/roguelike/util/ArgumentParser.java
@@ -3,13 +3,46 @@ package greymerk.roguelike.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import greymerk.roguelike.worldgen.Coord;
+
 public class ArgumentParser {
 
-  List<String> args;
+  private final List<String> args = new ArrayList<>();
 
   public ArgumentParser(List<String> args) {
-    this.args = new ArrayList<>();
     this.args.addAll(args);
+  }
+
+  public int getInt(int index) {
+    return parseInteger(get(index));
+  }
+
+  public String get(int index) {
+    return this.hasEntry(index) ? this.args.get(index) : null;
+  }
+
+  private int parseInteger(String input) {
+    try {
+      return Integer.parseInt(input);
+    } catch (NumberFormatException numberFormatException) {
+      throw new RuntimeException("Could not parse input as number: " + input);
+    }
+  }
+
+  public Coord getCoord(int argIndex) {
+    if (hasEntry(argIndex)) {
+      int arg1 = this.getInt(argIndex);
+      int arg2 = this.getInt(argIndex + 1);
+
+      if (!hasEntry(argIndex + 2)) {
+        return new Coord(arg1, 0, arg2);
+      } else {
+        int arg3 = this.getInt(argIndex + 2);
+        return new Coord(arg1, arg2, arg3);
+      }
+    }
+    // todo: internationalization
+    throw new IllegalArgumentException(String.format("Could not map arguments to coord: %s.", this));
   }
 
   public boolean hasEntry(int index) {
@@ -17,19 +50,7 @@ public class ArgumentParser {
   }
 
   public boolean match(int index, String toCompare) {
-
-    if (!this.hasEntry(index)) {
-      return false;
-    }
-    return this.args.get(index).equals(toCompare);
-  }
-
-  public String get(int index) {
-
-    if (!this.hasEntry(index)) {
-      return null;
-    }
-    return this.args.get(index);
+    return this.hasEntry(index) && this.args.get(index).equals(toCompare);
   }
 
   @Override
