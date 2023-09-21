34a56d725883df535cd4372d06d0130ff2dea5c0
Fnar <fnardottv@gmail.com>
2023-09-20 23:45:13 -0700

    Move argument parsing to ArgumentParser

--- 1.12/src/main/java/com/github/fnar/roguelike/command/ContextHolder1_12.java
+++ 1.12/src/main/java/com/github/fnar/roguelike/command/ContextHolder1_12.java
@@ -1,8 +1,6 @@
 package com.github.fnar.roguelike.command;
 
-import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
-import net.minecraft.command.NumberInvalidException;
 
 import org.apache.commons.lang3.NotImplementedException;
 
@@ -15,41 +13,16 @@ import greymerk.roguelike.worldgen.Coord;
 public class ContextHolder1_12 implements ContextHolder {
 
   private final CommandSender1_12 sender;
-  private final List<String> args;
+  private final ArgumentParser argumentParser;
 
   public ContextHolder1_12(ICommandSender sender, List<String> args) {
     this.sender = new CommandSender1_12(sender);
-    this.args = args;
+    this.argumentParser = new ArgumentParser(args);
   }
 
   @Override
   public Coord getArgumentAsCoord(String argumentName) {
-    ArgumentParser argumentParser = new ArgumentParser(args);
-    if (argumentParser.hasEntry(0)) {
-      int arg1 = getArgAsInt(argumentParser, 1);
-      int arg2 = getArgAsInt(argumentParser, 2);
-
-      if (!argumentParser.hasEntry(3)) {
-        return new Coord(arg1, 0, arg2);
-      } else {
-        int arg3 = getArgAsInt(argumentParser, 3);
-        return new Coord(arg1, arg2, arg3);
-      }
-    }
-    // todo: internationalization
-    throw new IllegalArgumentException(String.format("Could not map arguments to coord: %s.", argumentParser));
-  }
-
-  private static int getArgAsInt(ArgumentParser argumentParser, int index) {
-    return parseInteger(argumentParser.get(index));
-  }
-
-  private static int parseInteger(String input) {
-    try {
-      return CommandBase.parseInt(input);
-    } catch (NumberInvalidException e) {
-      throw new RuntimeException("Could not parse input as number: " + input);
-    }
+    return argumentParser.getCoord(1);
   }
 
   @Override
