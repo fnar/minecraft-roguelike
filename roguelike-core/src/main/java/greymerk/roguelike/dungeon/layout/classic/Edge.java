2e9c971c2c90b72cd65270b0e5f334b7ba7502b8
Fnar <fnardottv@gmail.com>
2023-09-27 23:08:40 -0700

    Simplify LayoutGeneratorClassic and its dependencies

--- roguelike-core/src/main/java/greymerk/roguelike/dungeon/layout/classic/Edge.java
+++ roguelike-core/src/main/java/greymerk/roguelike/dungeon/layout/classic/Edge.java
@@ -1,64 +1,52 @@
 package greymerk.roguelike.dungeon.layout.classic;
 
-import java.util.Random;
-
 import greymerk.roguelike.dungeon.layout.DungeonTunnel;
 import greymerk.roguelike.worldgen.Coord;
 import greymerk.roguelike.worldgen.Direction;
 
 public class Edge {
 
-  private final LayoutGeneratorClassic layoutGeneratorClassic;
   private final Coord start;
   private final Coord end;
   private final Direction dir;
-  private final int scatter;
-  private boolean done = false;
-  private int extensionsRemaining;
+  private boolean isDone = false;
+  private int extensionsRemaining = 2;
 
-  public Edge(LayoutGeneratorClassic layoutGeneratorClassic, Coord start, Direction dir) {
-    this.layoutGeneratorClassic = layoutGeneratorClassic;
+  public Edge(Coord start, Direction dir) {
     this.start = start.copy();
     this.end = start.copy();
     this.dir = dir;
-    this.scatter = layoutGeneratorClassic.getScatter();
-    this.extensionsRemaining = 2;
-  }
-
-  public void update(Random random) {
-    if (isDone()) {
-      return;
-    }
-
-    if (layoutGeneratorClassic.hasNodeNearby(end)) {
-      end.translate(getDir(), scatter);
-    } else if (random.nextInt(1 + extensionsRemaining) > 0) {
-      end.translate(getDir(), scatter);
-      extensionsRemaining--;
-    } else {
-      layoutGeneratorClassic.createNode(this);
-      this.done = true;
-    }
-  }
-
-  public boolean isDone() {
-    return done;
-  }
-
-  public Direction getDirection() {
-    return getDir();
-  }
-
-  public Coord getEnd() {
-    return end.copy();
   }
 
   public DungeonTunnel asDungeonTunnel() {
-    return new DungeonTunnel(start.copy(), end.copy());
+    return new DungeonTunnel(getStart().copy(), getEnd().copy());
+  }
+
+  public Coord getStart() {
+    return start;
+  }
+
+  public Coord getEnd() {
+    return end;
   }
 
   public Direction getDir() {
     return dir;
   }
 
+  public boolean isDone() {
+    return isDone;
+  }
+
+  public int getExtensionsRemaining() {
+    return extensionsRemaining;
+  }
+
+  public void setDone(boolean done) {
+    isDone = done;
+  }
+
+  public void setExtensionsRemaining(int extensionsRemaining) {
+    this.extensionsRemaining = extensionsRemaining;
+  }
 }
--- roguelike-core/src/main/java/greymerk/roguelike/dungeon/layout/classic/LayoutGeneratorClassic.java
+++ roguelike-core/src/main/java/greymerk/roguelike/dungeon/layout/classic/LayoutGeneratorClassic.java
@@ -1,10 +1,16 @@
 package greymerk.roguelike.dungeon.layout.classic;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import greymerk.roguelike.dungeon.layout.DungeonNode;
+import greymerk.roguelike.dungeon.layout.DungeonTunnel;
 import greymerk.roguelike.dungeon.layout.LayoutGenerator;
 import greymerk.roguelike.dungeon.layout.LevelLayout;
 import greymerk.roguelike.worldgen.Coord;
@@ -19,7 +25,7 @@ public class LayoutGeneratorClassic implements LayoutGenerator {
   private final int numRooms;
   private final int scatter;
   private final int range;
-  private List<Node> graphNodes;
+  private final Map<Node, Set<Edge>> graph = new HashMap<>();
 
   public LayoutGeneratorClassic(int numRooms, int scatter, int range) {
     layout = new LevelLayout();
@@ -29,36 +35,58 @@ public class LayoutGeneratorClassic implements LayoutGenerator {
   }
 
   public LevelLayout generate(Coord start, Random random) {
-    this.setStart(start);
-    Node startNode = new Node(this, start, Direction.randomCardinal(random));
-    List<Node> gNodes = generateGraph(startNode, random);
+    this.start = start;
+    Node node = createNode(start, Direction.randomCardinal(random));
 
-    return generateLayoutFromGraph(gNodes, startNode, random);
-  }
-
-  private List<Node> generateGraph(Node startNode, Random random) {
-    graphNodes = new ArrayList<>();
-    graphNodes.add(startNode);
-
-    while (!isDone(graphNodes)) {
-      update(graphNodes, random);
+    while (!isDone()) {
+      update(random);
     }
 
-    graphNodes.forEach(Node::cull);
+    getNodes().forEach(this::cull);
 
-    return graphNodes;
+    return generateLayoutFromGraph(node, random);
   }
 
-  private LevelLayout generateLayoutFromGraph(List<Node> graphNodes, Node startNode, Random random) {
+  private boolean isDone(Node node) {
+    return getEdges(node).stream().allMatch(Edge::isDone);
+  }
+
+  private List<DungeonTunnel> createTunnels(Node node) {
+    return getEdges(node).stream().map(Edge::asDungeonTunnel).collect(Collectors.toList());
+  }
+
+  private Set<Node> getNodes() {
+    return graph.keySet();
+  }
+
+  private Set<Edge> getEdges(Node node) {
+    return graph.get(node);
+  }
+
+  private Node createNode(Coord coord, Direction direction) {
+    Node node = new Node(coord, direction);
+    graph.put(node, new HashSet<>());
+
+    if (start.distance(coord) <= range) {
+      Direction.cardinals().stream()
+          .filter(dir -> !dir.equals(direction.reverse()))
+          .map(dir -> new Edge(coord.copy(), dir))
+          .forEach(edge -> graph.get(node).add(edge));
+    }
+
+    return node;
+  }
+
+  private LevelLayout generateLayoutFromGraph(Node startNode, Random random) {
     DungeonNode startDungeonNode = null;
 
-    for (Node node : graphNodes) {
-      DungeonNode dungeonNode = node.asDungeonNode();
+    for (Node node : getNodes()) {
+      DungeonNode dungeonNode = asDungeonNode(node);
       if (node == startNode) {
         startDungeonNode = dungeonNode;
       }
       layout.addNode(dungeonNode);
-      layout.addTunnels(node.createTunnels());
+      layout.addTunnels(createTunnels(node));
     }
 
     layout.setStartEnd(random, startDungeonNode);
@@ -66,52 +94,70 @@ public class LayoutGeneratorClassic implements LayoutGenerator {
     return layout;
   }
 
-  public void update(List<Node> nodes, Random random) {
-    if (!isFull(nodes)) {
-      for (int i = 0; i < nodes.size(); i++) {
-        nodes.get(i).update(random);
-      }
+  private DungeonNode asDungeonNode(Node node) {
+    return new DungeonNode(getEntrances(node), node.getCoord());
+  }
+
+  private List<Direction> getEntrances(Node node) {
+    List<Direction> entrances = new ArrayList<>();
+    entrances.add(node.getDirection().reverse());
+    getEdges(node).stream().map(Edge::getDir).forEach(entrances::add);
+    return entrances;
+  }
+
+  public void update(Random random) {
+    if (isFull()) {
+      return;
+    }
+    defeatConcurrentModification().forEach(node -> updateNode(random, node));
+  }
+
+  private HashSet<Node> defeatConcurrentModification() {
+    return new HashSet<>(getNodes());
+  }
+
+  private boolean isDone() {
+    return getNodes().stream().allMatch(this::isDone) || isFull();
+  }
+
+  private boolean isFull() {
+    return getNodes().size() >= Math.max(numRooms, MIN_ROOMS);
+  }
+
+  public void updateNode(Random random, Node node) {
+    getEdges(node).forEach(edge -> updateEdge(random, edge));
+  }
+
+  public void updateEdge(Random random, Edge edge) {
+    if (edge.isDone()) {
+      return;
+    }
+
+    if (hasNodeNearby(edge.getEnd())) {
+      edge.getEnd().translate(edge.getDir(), scatter);
+    } else if (random.nextInt(1 + edge.getExtensionsRemaining()) > 0) {
+      edge.getEnd().translate(edge.getDir(), scatter);
+      edge.setExtensionsRemaining(edge.getExtensionsRemaining() - 1);
+    } else {
+      createNode(edge.getEnd().copy(), edge.getDir());
+      edge.setDone(true);
     }
   }
 
-  private boolean isDone(List<Node> nodes) {
-    return nodes.stream().allMatch(Node::isDone) || isFull(nodes);
-  }
-
-  private boolean isFull(List<Node> nodes) {
-    return nodes.size() >= Math.max(numRooms, MIN_ROOMS);
-  }
-
-  void createNode(Edge edge) {
-    graphNodes.add(new Node(this, edge.getEnd(), edge.getDirection()));
-  }
-
-  boolean hasNodeNearby(Coord coord) {
-    return graphNodes.stream()
-        .map(Node::getPos)
+  private boolean hasNodeNearby(Coord coord) {
+    return getNodes().stream()
+        .map(Node::getCoord)
         .mapToInt(coord::distanceAsInt)
         .anyMatch(distance -> distance < scatter);
+  }
+
+  private void cull(Node node) {
+    graph.put(node, graph.get(node).stream().filter(Edge::isDone).collect(Collectors.toSet()));
   }
 
   @Override
   public LevelLayout getLayout() {
     return layout;
-  }
-
-  public int getScatter() {
-    return scatter;
-  }
-
-  public Coord getStart() {
-    return start;
-  }
-
-  public void setStart(Coord start) {
-    this.start = start;
-  }
-
-  public int getRange() {
-    return range;
   }
 
 }
