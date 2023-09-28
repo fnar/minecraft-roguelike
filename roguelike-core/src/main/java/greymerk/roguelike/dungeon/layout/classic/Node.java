74c91104d0850459e2794085a0ea7858b80f6423
Fnar <fnardottv@gmail.com>
2023-09-27 23:08:40 -0700

    Simplify LayoutGeneratorClassic and its dependencies

--- roguelike-core/src/main/java/greymerk/roguelike/dungeon/layout/classic/Node.java
+++ roguelike-core/src/main/java/greymerk/roguelike/dungeon/layout/classic/Node.java
@@ -1,67 +1,24 @@
 package greymerk.roguelike.dungeon.layout.classic;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-
-import greymerk.roguelike.dungeon.layout.DungeonNode;
-import greymerk.roguelike.dungeon.layout.DungeonTunnel;
 import greymerk.roguelike.worldgen.Coord;
 import greymerk.roguelike.worldgen.Direction;
 
 public class Node {
 
-  private final Coord pos;
+  private final Coord coord;
   private final Direction direction;
-  private List<Edge> edges;
 
-  public Node(LayoutGeneratorClassic layoutGeneratorClassic, Coord pos, Direction direction) {
-//    System.out.printf("Generating node at %s%n", pos);
-    edges = new ArrayList<>();
-    this.pos = pos;
+  public Node(Coord coord, Direction direction) {
+    this.coord = coord;
     this.direction = direction;
-
-    if (layoutGeneratorClassic.getStart().distance(this.pos) <= layoutGeneratorClassic.getRange()) {
-      Direction.CARDINAL.stream()
-          .filter(dir -> !dir.equals(this.direction.reverse()))
-          .map(dir -> new Edge(layoutGeneratorClassic, this.pos.copy(), dir))
-          .forEach(edge -> edges.add(edge));
-    }
   }
 
-  public void update(Random random) {
-    edges.forEach(edge -> edge.update(random));
+  public Coord getCoord() {
+    return coord.copy();
   }
 
-  public boolean isDone() {
-    return edges.stream().allMatch(Edge::isDone);
+  public Direction getDirection() {
+    return direction;
   }
 
-  public Coord getPos() {
-    return pos.copy();
-  }
-
-  public List<Direction> getEntrances() {
-    List<Direction> entrances = new ArrayList<>();
-    entrances.add(direction.reverse());
-    edges.stream().map(Edge::getDir).forEach(entrances::add);
-    return entrances;
-  }
-
-  public List<Edge> getEdges() {
-    return edges;
-  }
-
-  public List<DungeonTunnel> createTunnels() {
-    return getEdges().stream().map(Edge::asDungeonTunnel).collect(Collectors.toList());
-  }
-
-  public DungeonNode asDungeonNode() {
-    return new DungeonNode(getEntrances(), pos);
-  }
-
-  public void cull() {
-    edges = edges.stream().filter(Edge::isDone).collect(Collectors.toList());
-  }
 }
